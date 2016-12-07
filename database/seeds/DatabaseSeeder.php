b8313446a1bb1f19bda18c4073126c446035567f
kamalnayak86 <knayak86@aecordigital.com>
2016-12-07 08:00:42 +0000

    no check

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,21 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+
+        //disable foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
-        //$this->call(UsersTableSeeder::class);        
+        $this->call(PeopleTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(AgeGroupsTableSeeder::class);
         $this->call(VenuesTableSeeder::class);
         $this->call(ClubsTableSeeder::class);
         $this->call(PitchesTableSeeder::class);
-        $this->call(TeamsTableSeeder::class); 
+        $this->call(TeamsTableSeeder::class);
         $this->call(TournamentsTableSeeder::class);
         $this->call(AgeGroupTournamentTableSeeder::class);
         $this->call(CompetitionsTableSeeder::class);
         $this->call(TeamCompetitionTableSeeder::class);
         $this->call(RefereeTableSeeder::class);
         $this->call(MatchResultsTableSeeder::class);
-        $this->call(FixturesTableSeeder::class);  
+        $this->call(FixturesTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
