a2b52622233c0bc019c02276c8a1fc3a6b40ee1e
kamalnayak86 <knayak86@aecordigital.com>
2016-12-08 10:16:26 +0000

    Add changes to Provider file and move to Api

--- app/Api/Controllers/TeamController.php
+++ app/Api/Controllers/TeamController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 // Need to Define Only Contracts
 use App\Api\Contracts\TeamContract;
 
+/**
+ * Teams Resource Description
+ *
+ * @Resource("Teams")
+ */
 class TeamController extends BaseController
 {
     public function __construct(TeamContract $teamObj)
@@ -16,11 +21,31 @@ class TeamController extends BaseController
         $this->teamObj = $teamObj;
     }
 
+   /**
+    * Show all Team Details
+    *
+    * Get a JSON representation of all the Teams.
+    *
+    * @Get("/teams")
+    * @Versions({"v1"})
+    * @Response(200, body={"id": 10, "username": "foo"})
+    */
+
     public function getTeams()
     {
         return $this->teamObj->getAllTeams();
     }
 
+    /**
+     * Create  team
+     *
+     * Create New Team 
+     *
+     * @Post("/team/create")
+     *
+     * @Versions({"v1"})
+     * @Request("name=test", contentType="application/x-www-form-urlencoded")
+     */
     public function createTeam(Request $request)
     {
         return $this->teamObj->createTeam($request);
