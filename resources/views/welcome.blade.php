470298f2d3ff09b15415c7cc275c741f2f5ff49b
Kamal Nayak <knayak@aecordigital.com>
2016-12-19 18:53:03 +0530

    Add changes for Route change using vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,17 @@
   <!-- route outlet -->
   <!-- component matched by the route will render here -->
     <div class="flex-center position-ref full-height">
-    		
         <div class="content">
             <div class="text-center">
-                <h1>Euro Sportirng</h1>    
+                <h1>Euro Sporting</h1>    
                   <router-link to="/top">Top</router-link>
                    <router-link to="/foo">Foo</router-link>
                   <router-link to="/bar">Bar</router-link>            
                 <router-view></router-view>      
             </div>
-
          </div>
-         
-       
-           
     </div>
+    
 @endsection
 
 @section('pageStyle')
