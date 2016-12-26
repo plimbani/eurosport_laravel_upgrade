4099c1208a48f6808f2a35079fb3bd888d4798d9
Kamal Nayak <knayak@aecordigital.com>
2016-12-26 16:25:44 +0530

    add change

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repositories;
 
 use App\Models\User;
+
 class UserRepository
+{
     public function getAllUsers()
     {
         /*  Use Join for Fetch Club Income Data with Club Table */
