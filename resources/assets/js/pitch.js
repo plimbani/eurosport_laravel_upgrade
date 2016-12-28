4539ff71aac3f3b389969d76b457cad875315330
Krunal <kparikh@aecordigital.com>
2016-12-28 11:40:19 +0530

    Changes

--- resources/assets/js/pitch.js
+++ resources/assets/js/pitch.js
@@ -1,18 +1,26 @@
-// require('./bootstrap');
+require('./bootstrap');
 
 import Vue from 'vue'
 
-const vuePitch= new Vue({
-	        el: "#pitchSet",
-            data: {
-                		pitchData: [],
-                		pitchDays:10
-
-                  },
-	      ready: function() {
-	      	alert('hi');
-	         console.log('msg');
-	        
-	      }
+vuePitch= new Vue({
+    el: "#pitchSet",
+    data: {
+        		pitchData: [],
+        		pitchDays:10
+          },
+    mounted: function() {
+      	this.getAvailability();	
+    },
+      methods: {
+        getAvailability: function() {
+            alert('Hello');
+            ajaxCall("pitch/get_data", data, 'GET', 'json', pitchDataSuccess);
+        },
+    }
 });
 
+$(document).ready(function(){
+    function pitchDataSuccess(Data, status, xhr) {
+        console.log(Data);
+    }
+})
