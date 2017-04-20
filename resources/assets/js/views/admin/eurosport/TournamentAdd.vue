6e5386f948c83379208964525946a4c061a73a35
Kamal Nayak <knayak@aecordigital.com>
2017-04-20 17:58:22 +0530

    resovle issue for validation for location

--- resources/assets/js/views/admin/eurosport/TournamentAdd.vue
+++ resources/assets/js/views/admin/eurosport/TournamentAdd.vue
@@ -122,8 +122,92 @@
                             >
                         </div>
                     </div>
-                    <location :locations="locations"></location>
-                    <div class="row">
+                    <!--<location :locations="locations"></location>-->
+                    <div v-for="(location, index) in locations">
+                      <div class="">
+                      <h6><strong>{{$lang.tournament_location}}</strong></h6>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_venue}}*</label>
+
+                        <div class="col-sm-4">
+                          <input type="text" class="form-control" placeholder="" 
+                          :name="'tournament_validation_venue'+index"
+                           v-model="location.tournament_venue_name" v-validate="'required'" 
+                           :class="{'is-danger':errors.has('tournament_validation_venue'+index) }">
+                           <i v-show="errors.has('tournament_validation_venue'+index)" class="fa fa-warning">
+                           </i>
+                           <span class="help is-danger" 
+                           v-show="errors.has('tournament_validation_venue'+index)">{{$lang.tournamemt_validation_venue}}
+                           </span>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_address}}</label>
+
+                      <div class="col-sm-4">
+                      <input type="text" class="form-control" 
+                      v-model="location.touranment_venue_address"
+                      placeholder="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_town_city}}</label>
+
+                      <div class="col-sm-4">
+                      <input type="text" class="form-control" 
+                      v-model="location.tournament_venue_city"
+                      placeholder="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_postcode}}</label>
+
+                      <div class="col-sm-4">
+                      <input type="text" class="form-control" 
+                      v-model="location.tournament_venue_postcode"
+                      placeholder="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_state}}</label>
+
+                      <div class="col-sm-4">
+                      <input type="text" class="form-control"
+                      v-model="location.tournament_venue_state"
+                      placeholder="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_country}}</label>
+
+                      <div class="col-sm-4">
+                      <div class="form-group">
+                      <select class="form-control ls-select2" v-model="location.tournament_venue_country">
+                      <option value="">{{$lang.tournament_country_please_select}}</option>
+                      <option value="Andorra">Andorra</option>
+                      <option value="Belgium">Belgium</option>
+                      <option value="France">France </option>
+                      <option value="Germany">Germany</option>
+                      <option value="Italy">Italy</option>
+                      <option value="Spain">Spain</option>
+                      <option value="United Kingdom">United Kingdom</option></select>
+                      </div>
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label class="col-sm-2 form-control-label">{{$lang.tournament_organiser}}</label>
+                      <div class="col-sm-4">
+                      <input type="text" class="form-control" placeholder="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <div class="col-sm-3">
+                      <button class="btn btn-danger w-75" @click.prevent="removeLocation(index)" v-if="index > 0">- Remove location</button>
+                      </div>
+                      </div>
+                      </div>
+                                          <div class="row">
                         <div class="col-sm-3">
                           <button class="btn btn-success w-75" @click.prevent="addLocationClick"><small><i class="jv-icon jv-plus"></i></small>&nbsp;{{$lang.tournament_location_button}}</button>
                         </div>
@@ -162,7 +246,8 @@ export default {
           tournament_venue_city: "",
           tournament_venue_postcode: "",
           tournament_venue_state: "",
-          tournament_venue_country: ""
+          tournament_venue_country: "",
+          tournament_location_id:0
         }
       ],
       image:'',
@@ -207,7 +292,8 @@ $('#btnSelect').on('click',function(){
                         tournament_venue_city: locations[i]['city'],
                         tournament_venue_postcode: locations[i]['postcode'],
                         tournament_venue_state: locations[i]['state'],
-                        tournament_venue_country: locations[i]['country']
+                        tournament_venue_country: locations[i]['country'],
+                        tournament_location_id: locations[i]['id'],
                     });
                   }
               }
@@ -303,12 +389,15 @@ $('#btnSelect').on('click',function(){
       this.image = '';
       e.preventDefault();
     },
+    removeLocation (index){
+        this.locations.splice(index,1)
+    },
     next() {
-      alert('Hello1234')
+      
       // this.handleValidation()
       // First Validate it
       // SET The Date Value for tournament
-      console.log(this.tournament)
+     
       this.$validator.validateAll().then(
           (response) => {
             // if its return true then proceed
