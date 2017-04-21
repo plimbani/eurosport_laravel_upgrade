b2d7bc49b0a740f51064f816cae5b2c30fced3cf
Kamal Nayak <knayak@aecordigital.com>
2017-04-21 15:10:41 +0530

    add change for last_active_time

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,21 +21,21 @@ class UsersTableSeeder extends Seeder
             ['person_id' => '1', 'username' => 'superadmin', 'user_image' => '1491572922.png',
             'name' => 'SuperAdmin', 'email' => 'superadmin@superadmin.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1', 'is_verified' => '1', 'timezone' => '', 
-            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0, 'last_active_time' => '', 
+            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0,  
             'is_blocked' => 0, 'is_mobile_user' => 0, 'blocked_time' => '', 'blocker_id' => 0,
             'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             
             ['person_id' => '2', 'username' => 'superadmin1', 'user_image' => '1491572922.png',
             'name' => 'SuperAdmin', 'email' => 'admin@admin.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1', 'is_verified' => '1', 'timezone' => '', 
-            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0, 'last_active_time' => '', 
+            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0,  
             'is_blocked' => 0, 'is_mobile_user' => 0, 'blocked_time' => '', 'blocker_id' => 0,
             'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
 
             ['person_id' => '3','username' => 'superadmin2', 'user_image' => '1491572922.png',
             'name' => 'SuperAdmin', 'email' => 'moderator@moderator.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1', 'is_verified' => '1', 'timezone' => '', 
-            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0, 'last_active_time' => '', 
+            'is_online' => 0, 'last_login_time' => '', 'is_active' => 0, 
             'is_blocked' => 0, 'is_mobile_user' => 0, 'blocked_time' => '', 'blocker_id' => 0,
             'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
         ]);
