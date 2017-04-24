3a60820543f3722ad291c860cdf4fb74b634c041
rishabh <rishabh>
2017-04-24 15:41:08 +0530

    changes

--- resources/assets/js/views/admin/eurosport/TournamentTeamGroup.vue
+++ resources/assets/js/views/admin/eurosport/TournamentTeamGroup.vue
@@ -1,22 +1,21 @@
-aa<template> 
+<template> 
   <div class="tab-content">
   	<div class="card">
   		<div class="card-block">
   			<h6><strong>{{$lang.teams_terms_groups}}</strong></h6>
   			<form>
   				<div class="form-group row">
-                  <label class="col-sm-2 form-control-label">{{$lang.teams_age_category}}</label>
-
-                    <div class="col-sm-4">
-                        <div class="form-group">
-                            <select class="form-control ls-select2" v-model="age_category" v-on:change="onSelectAgeCategory">
-	                            <option value="">{{$lang.teams_select_age_category}}</option>
-	                            <option v-for="option in options" 
-                               v-bind:value="option"> {{option.group_name}}</option>
-	                        </select>
-	                    </div>
-                    </div>
-                </div>
+            <label class="col-sm-2 form-control-label">{{$lang.teams_age_category}}</label>
+            <div class="col-sm-4">
+              <div class="form-group">
+                  <select class="form-control ls-select2" v-model="age_category" v-on:change="onSelectAgeCategory">
+                    <option value="">{{$lang.teams_select_age_category}}</option>
+                    <option v-for="option in options" 
+                     v-bind:value="option"> {{option.group_name}}</option>
+                  </select>
+              </div>
+            </div>
+          </div>
   			</form>
         <div class="block-bg age-category">
           <div class="d-flex justify-content-center align-items-center">
@@ -52,15 +51,12 @@ aa<template>
   				<div class="pull-left">
 	  				<div class="mt-4"><strong>{{$lang.teams_team_list}}</strong></div>
             <form method="post" name="frmCsvImport" id="frmCsvImport" enctype="multipart/form-data">
-
-            <div >
-            
+            <div>
               <input type="file" name="fileUpload"  id="fileUpload" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" >
               <p class="help-block">Only excel and csv allowed.</p>
             </div>
 	  				<button type="button" @click="csvImport()" :disabled="age_category==''"  class="btn btn-primary">{{$lang.teams_upload_team}}</button>
             </form>
-            
 	  			</div>
 	  			<div class="pull-right mt-4">
 	  				<form  class="form-inline filter-category-form" >
@@ -68,26 +64,25 @@ aa<template>
 	  						<label for="nameInput" class="control-label"><strong>{{$lang.teams_filter}}</strong></label> 
 	  						<label class="radio-inline control-label">
   								<input type="radio" name="gender" value="team" checked="checked">{{$lang.teams_team}}
-                            </label>
-                             <label class="radio-inline control-label">
-                            	<input type="radio" name="gender" value="country">{{$lang.teams_country}}
-                            </label>
-                            <label class="radio-inline control-label">
-                            	<input type="radio" name="gender" value="age category">{{$lang.teams_age}}
-                            </label>
-                            <select class="form-control ls-select2">
-	                            <option value="">{{$lang.teams_select_location}}</option>
-	                            <option value="">{{$lang.teams_select_location_1}}</option>
-	                            <option value="">{{$lang.teams_select_location_2}}</option>
-	                            <option value="">{{$lang.teams_select_location_3}}</option>
-	                            <option value="">{{$lang.teams_select_location_4}}</option>
-	                            <option value="">{{$lang.teams_select_etc}}</option>
-	                        </select>
-	                        <label class="control-label">
-	                        	<a href="#">{{$lang.teams_clear}}</a>
-	                        </label>
+                </label>
+                <label class="radio-inline control-label">
+                  	<input type="radio" name="gender" value="country">{{$lang.teams_country}}
+                </label>
+                <label class="radio-inline control-label">
+                  	<input type="radio" name="gender" value="age category">{{$lang.teams_age}}
+                </label>
+                <select class="form-control ls-select2">
+                    <option value="">{{$lang.teams_select_location}}</option>
+                    <option value="">{{$lang.teams_select_location_1}}</option>
+                    <option value="">{{$lang.teams_select_location_2}}</option>
+                    <option value="">{{$lang.teams_select_location_3}}</option>
+                    <option value="">{{$lang.teams_select_location_4}}</option>
+                    <option value="">{{$lang.teams_select_etc}}</option>
+                </select>
+                <label class="control-label">
+                	<a href="#">{{$lang.teams_clear}}</a>
+                </label>
 	  					</div> 
-	  					
 	  				</form>
 	  			</div>
   			</div>
@@ -95,36 +90,33 @@ aa<template>
   				<div class="col-md-12">
           <form name="frmTeamAssign" id="frmTeamAssign">
   					<table class="table add-category-table">
-                        <thead>
-                            <tr >
-                                <th>{{$lang.teams_reference}}</th>
-                                <th>{{$lang.teams_name}}</th>
-                                <th>{{$lang.teams_country}}</th>
-                                <th>{{$lang.teams_agecategory}}</th>
-                                <th>{{$lang.teams_group}}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr :id="team.team_id" v-for="team in teams">
-                              <td>{{team.esr_reference}}</td>
-                              <td>{{team.name}}</td>
-                              <td>
-                              	<img :src="team.logo" width="20">{{team.country_name}} 
-                              </td>
-                              <td>{{team.age_name}} </td>
-                              <td>
-                                <select  v-bind:data-id="team.id" v-model="team.group_name"  v-bind:data-name.once="initialfunc(team.id)" v-bind:data-category-name="age_category.group_name" v-on:focus="beforeChange(team.id)" v-on:change="onAssignGroup(team.id)"  :name="'sel_'+team.id" :id="'sel_'+team.id" class="form-control ls-select2 selTeams">
-			                            <option value="">Select Team</option>
-                                  <optgroup :label="group.groups.group_name" v-for="group in grps">
-                                    <option :class="'sel_'+team.id" v-for="(n,index) in group['group_count']" :disabled="isSelected(group['groups']['group_name'],n)"  :value="group['groups']['group_name']+n" >{{group['groups']['group_name']}}{{n}} </option>
-                                  </optgroup>
-                               
-		                            </select>
-                              </td>
-                            </tr>
-                           
-
-                        </tbody>
+              <thead>
+                  <tr>
+                      <th>{{$lang.teams_reference}}</th>
+                      <th>{{$lang.teams_name}}</th>
+                      <th>{{$lang.teams_country}}</th>
+                      <th>{{$lang.teams_agecategory}}</th>
+                      <th>{{$lang.teams_group}}</th>
+                  </tr>
+              </thead>
+                <tbody>
+                    <tr :id="team.team_id" v-for="team in teams">
+                      <td>{{team.esr_reference}}</td>
+                      <td>{{team.name}}</td>
+                      <td>
+                      	<img :src="team.logo" width="20">{{team.country_name}} 
+                      </td>
+                      <td>{{team.age_name}} </td>
+                      <td>
+                        <select  v-bind:data-id="team.id" v-model="team.group_name"  v-bind:data-name.once="initialfunc(team.id)" v-bind:data-category-name="age_category.group_name" v-on:focus="beforeChange(team.id)" v-on:change="onAssignGroup(team.id)"  :name="'sel_'+team.id" :id="'sel_'+team.id" class="form-control ls-select2 selTeams">
+                          <option value="">Select Team</option>
+                          <optgroup :label="group.groups.group_name" v-for="group in grps">
+                            <option :class="'sel_'+team.id" v-for="(n,index) in group['group_count']" :disabled="isSelected(group['groups']['group_name'],n)"  :value="group['groups']['group_name']+n" >{{group['groups']['group_name']}}{{n}} </option>
+                          </optgroup> 
+                        </select>
+                      </td>
+                    </tr>
+                </tbody>
             </table>
             <button type="button" @click="groupUpdate()" class="btn btn-primary pull-right">{{$lang.teams_button_updategroups}}</button>
           </form>  
