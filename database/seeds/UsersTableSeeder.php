c0a54c8afc645fb2209a4e39ebd85049fd50686e
rishabh <rishabh>
2017-05-03 17:22:56 +0530

    UsersSeeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,20 +17,20 @@ class UsersTableSeeder extends Seeder
         $people = DB::table('people')->take(3)->select('id')->get()->toArray();
       
         DB::table('users')->insert([
-            ['person_id' => '3','username' => 'TournamentAdministrator', 'user_image' => '1491572922.png',
+            ['person_id' => '3','username' => 'TournamentAdministrator', 
             'name' => 'TournamentAdministrator', 'email' => 'tadministrator@administrator.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1','timezone' => '', 'is_mobile_user' => '0', 'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             
-            ['person_id' => '4','username' => 'moderator', 'user_image' => '1491572922.png',
+            ['person_id' => '4','username' => 'moderator', 
             'name' => 'moderator', 'email' => 'moderator@moderator.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1','timezone' => '', 'is_mobile_user' => '0', 'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             
-            ['person_id' => '2', 'username' => 'master', 'user_image' => '1491572922.png',
+            ['person_id' => '2', 'username' => 'master', 
             'name' => 'master', 'email' => 'master@master.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1', 'timezone' => '', 'is_mobile_user' => '0', 'settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
 
 
-            ['person_id' => '1', 'username' => 'superadmin', 'user_image' => '1491572922.png',
+            ['person_id' => '1', 'username' => 'superadmin', 
             'name' => 'SuperAdmin', 'email' => 'superadmin@superadmin.com', 'organisation' => 'Euro-Sportring',
             'password' => bcrypt('password'), 'token' => '1','timezone' => '','is_mobile_user' => '0','settings' => '', 'remember_token' => '', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
         ]);
