f77fa3abeab908dcb377a07d9c93946547e17cda
krunal <kparikh@aecordigital.com>
2017-05-10 15:05:25 +0530

    Add changes in team upload

--- resources/assets/js/api/siteconfig.js
+++ resources/assets/js/api/siteconfig.js
@@ -1,11 +1,36 @@
 import axios from 'axios'
-
+import NProgress from 'nprogress'
+// var NProgress = require("nprogress")
+// var PulseLoader = require('vue-spinner/src/PulseLoader.vue');
 // EndPoint API
+import { PulseLoader } from 'vue-spinner/dist/vue-spinner.min.js'
 
 var instance = axios.create({
+  // baseURL: 'http://krunal-eurosport.dev.aecortech.com/api/',
   baseURL: 'http://esr.aecordigitalqa.com/api/',
+  
   timeout: 50000
 })
 
+instance.interceptors.request.use(function (config) {
+   	NProgress.start();
+
+  	return config
+}, function (error) {
+  
+  return Promise.reject(error)
+})
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+  
+  NProgress.done(true);
+  NProgress.remove();
+
+  return response
+}, function (error) {
+  // Loading.hide()
+  // alert('hi1233454345')
+  return Promise.reject(error)
+})
 
 export default instance
