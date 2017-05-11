f084517d481f99a767e8b4215179e43471f4faae
Kamal Nayak <knayak@aecordigital.com>
2017-05-11 11:16:36 +0530

    add change for reset blade

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -30,88 +30,90 @@
 </head>
 <body class="login-page pace-done">
 <div id="app" class="template-container">
-    
-        <div class="login-wrapper">
-            <div class="login-box">
-                <div class="brand-main">
-                    <a href="/admin">
-                        <!-- <img src="http://www.euro-sportring.com/sites/default/files/euro-sportring_1.png" alt="Laraspace Logo"> -->
-                        <img src="/assets/img/logo-desk.svg" alt="Laraspace Logo">
-                    </a>
-                </div>
-                
-                <div class="row">
-                        <div class="col-md-12">
-                            <div class="panel panel-default reset-password">
-                                <div class="panel-heading">
-                                    <p class="h4 text-center mt-4">Reset password</p>
-                                </div>
 
-                                <div class="panel-body">
-                                    @if (session('status'))
-                                        <div class="alert alert-success">
-                                            {{ session('status') }}
-                                        </div>
-                                    @endif
+<div class="login-wrapper">
+  <div class="login-box">
+      <div class="brand-main">
+          <a href="/admin">
+              <!-- <img src="http://www.euro-sportring.com/sites/default/files/euro-sportring_1.png" alt="Laraspace Logo"> -->
+              <img src="/assets/img/logo-desk.svg" alt="Laraspace Logo">
+          </a>
+      </div>
 
-                                    <form role="form" method="POST" action="{{ route('password.request') }}">
-                                        {{ csrf_field() }}
+      <div class="row">
+              <div class="col-md-12">
+                  <div class="panel panel-default reset-password">
+                      <div class="panel-heading">
+                          <p class="h4 text-center mt-4">Reset password</p>
+                      </div>
 
-                                        <input type="hidden" name="token" value="{{ $token }}">
+                      <div class="panel-body">
+                          @if (session('status'))
+                              <div class="alert alert-success">
+                                  {{ session('status') }}
+                              </div>
+                          @endif
 
-                                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                            <label for="email">Email address</label>
-                                            <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-                                            @if ($errors->has('email'))
-                                                <!-- <span class="help-block">
-                                                    <strong>{{ $errors->first('email') }}</strong>
-                                                </span> -->
-                                                <small class="form-text text-danger">{{ $errors->first('email') }}</small>
-                                            @endif
-                                        </div>
+                          <form role="form" method="POST"
+                          action="{{ route('password.request') }}">
+                              {{ csrf_field() }}
 
-                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                            <label for="password">Password</label>
-                                            <input id="password" type="password" class="form-control" name="password" required>
+                              <input type="hidden" name="token" value="{{ $token }}">
 
-                                            @if ($errors->has('password'))
-                                                <!-- <span class="help-block">
-                                                    <strong>{{ $errors->first('password') }}</strong>
-                                                </span> -->
-                                                <small class="form-text text-danger">{{ $errors->first('password') }}</small>
-                                            @endif
-                                        </div>
+                              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                  <label for="email">Email address</label>
+                                  <input id="email" type="email" class="form-control" name="email"
+                                   value="{{ $email or old('email') }}" required autofocus>
+                                  @if ($errors->has('email'))
+                                       <span class="help-block">
+                                          <strong>{{ $errors->first('email') }}</strong>
+                                      </span>
+                                      <small class="form-text text-danger">{{ $errors->first('email') }}</small>
+                                  @endif
+                              </div>
 
-                                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                            <label for="password-confirm">Confirm password</label>
-                                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                              <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                  <label for="password">Password</label>
+                                  <input id="password" type="password" class="form-control" name="password" required>
 
-                                            @if ($errors->has('password_confirmation'))
-                                                <!-- <span class="help-block">
-                                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                                </span> -->
-                                                <small class="form-text text-danger">{{ $errors->first('password_confirmation') }}</small>
-                                            @endif
-                                        </div>
+                                  @if ($errors->has('password'))
+                                       <span class="help-block">
+                                          <strong>{{ $errors->first('password') }}</strong>
+                                      </span>
+                                      <small class="form-text text-danger">{{ $errors->first('password') }}</small>
+                                  @endif
+                              </div>
 
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary">
-                                                Reset Password
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-                 
-                <div class="page-copyright">
-                    <p>Copyright 2017 Euro-Sportring. All rights reserved.</br>
-                    Developed  by <a href="http://aecordigital.com" target="_blank">aecor </a></p>
-                </div>
-            </div>
+                              <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                  <label for="password-confirm">Confirm password</label>
+                                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                                  @if ($errors->has('password_confirmation'))
+                                       <span class="help-block">
+                                          <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                      </span>
+                                      <small class="form-text text-danger">{{ $errors->first('password_confirmation') }}</small>
+                                  @endif
+                              </div>
+
+                              <div class="form-group text-center">
+                                  <button type="submit" class="btn btn-primary">
+                                      Reset Password
+                                  </button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+      </div>
+
+        <div class="page-copyright">
+            <p>Copyright 2017 Euro-Sportring. All rights reserved.</br>
+            Developed  by <a href="http://aecordigital.com" target="_blank">aecor </a></p>
         </div>
-   
+    </div>
+</div>
+
 </div>
 </body>
 
