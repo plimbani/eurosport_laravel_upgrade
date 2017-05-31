f6fb54b721b1056f920f6541ceceaf115126d6b8
Kamal Nayak <knayak@aecordigital.com>
2017-05-31 12:06:01 +0530

    remove blank message

--- resources/assets/js/services/auth.js
+++ resources/assets/js/services/auth.js
@@ -39,7 +39,10 @@ export default {
     check(){
         return axios.get('/api/auth/check').then(response =>  {
             if(response.data.authenticated == false) {
-              toastr['error'](response.data.message, 'Error');
+
+              if(response.data.message != undefined) {
+                toastr['error'](response.data.message, 'Error');
+              }
             }
             return !!response.data.authenticated;
         }).catch(error => {
