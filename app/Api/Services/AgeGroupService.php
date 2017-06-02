154bc5028b0391cc21b06c3072ea103d14e80c9c
Kamal Nayak <knayak@aecordigital.com>
2017-06-02 16:12:37 +0530

    add change

--- app/Api/Services/AgeGroupService.php
+++ app/Api/Services/AgeGroupService.php
@@ -151,7 +151,7 @@ class AgeGroupService implements AgeGroupContract
 
         // $disp_format_name = $json_data->tournament_teams .' TEAMS,'. $json_data->competation_format;
         $disp_format_name = $json_data->tournament_teams .' teams: '.
-        $json_data->competition_group_round.'-'.$json_data->competition_round;
+        $json_data->competition_group_round.' - '.$json_data->competition_round;
 
         $total_matches = $json_data->total_matches;
 
