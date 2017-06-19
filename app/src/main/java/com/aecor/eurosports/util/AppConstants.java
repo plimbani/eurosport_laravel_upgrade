144046ed4dc31b8a47f47e2e7bafcdf761995c52
anjalisoni-aecor <asoni@aecordigital.com>
2017-06-19 16:54:59 +0530

    Updating code

--- app/src/main/java/com/aecor/eurosports/util/AppConstants.java
+++ app/src/main/java/com/aecor/eurosports/util/AppConstants.java
@@ -5,6 +5,11 @@ package com.aecor.eurosports.util;
  */
 public class AppConstants {
     public static int SPLASH_TIME_OUT = 1000;
-
+    public static String FACEBOOK_URL = "https://www.facebook.com/BakeUrCakes";
+    public static String FACEBOOK_PAGE_ID = "BakeUrCakes";
+    public static String INSTAGRAM_APP_URL = "http://instagram.com/_u/im_curren";
+    public static String INSTAGRAM_URL = "http://instagram.com/im_curren";
+    public static String TWITTER_APP_URL = "twitter://user?screen_name=karan_deopura";
+    public static String TWITTER_URL = "https://twitter.com/karan_deopura";
 }
 
