16dd3d5c7384b8686b6f543b0e5c1307e829f426
Kamal Nayak <knayak@aecordigital.com>
2017-06-22 18:45:42 +0530

    add change for results

--- app/Api/Repositories/MatchRepository.php
+++ app/Api/Repositories/MatchRepository.php
@@ -559,10 +559,10 @@ class MatchRepository
     }
     public function updateScore($matchData)
     {
-      $scores = explode('-', $matchData['score']);
+      //$scores = explode('-', $matchData['score']);
       $updateData = [
-        'hometeam_score' => $scores[0],
-        'awayteam_score' => $scores[1]
+        'hometeam_score' => $matchData['home_score'],
+        'awayteam_score' => $matchData['away_score']
       ];
       return DB::table('temp_fixtures')
             ->where('id', $matchData['matchId'])
