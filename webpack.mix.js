d1f42893dec96fd93f99b845713e135193b635b4
Kamal Nayak <knayak@aecordigital.com>
2017-06-23 12:00:25 +0530

    resolve error

--- webpack.mix.js
+++ webpack.mix.js
@@ -42,10 +42,18 @@ mix.combine([
     pluginPath + 'alertify/alertify.js',
     pluginPath + 'easypiecharts/jquery.easypiechart.js',
     pluginPath + 'metisMenu/metisMenu.js',
-    pluginPath + 'fullcalendar/lib/moment.min.js',
-    pluginPath + 'fullcalendar/fullcalendar.js',
-    pluginPath + 'fullcalendar-scheduler/scheduler.js',
-    pluginPath + 'fullcalendar/draggable.js'
+
+     pluginPath + 'fullcalendar/lib/moment.min.js',
+     pluginPath + 'fullcalendar/fullcalendar.js',
+     pluginPath + 'fullcalendar-scheduler/scheduler1.js',
+     pluginPath + 'fullcalendar/draggable.js'
+  //  pluginPath + 'fullcalendar/lib/moment.min.js',
+  //  pluginPath + 'fullcalendar-scheduler/lib/jquery-ui.min.js',
+  //  pluginPath + 'fullcalendar/fullcalendar.js',
+  //  pluginPath + 'fullcalendar-scheduler/lib/scheduler.js'
+
+
+
 ],'public/assets/js/core/plugins.js')
 
 .js('resources/assets/js/app.js','public/assets/js/')
