2b9c2d65829329615cdfbca6c3e3dca4c2f7c3f9
krunal <kparikh@aecordigital.com>
2017-06-30 14:32:05 +0530

    Add changes

--- resources/assets/js/components/GamesTab.vue
+++ resources/assets/js/components/GamesTab.vue
@@ -6,17 +6,19 @@
               {{$lang.pitch_planner_no_games}}
         </div>
         <div class="text-center" v-else v-for="(competition,index) in competitionWithGames">
+          <div v-if="competition.matchList.length > 0">
+            <h6 class="mb-0" ><strong>{{competition.group_name}}</strong></h6>
 
-          <h6 class="mb-0" v-if="competition.matchList.length > 0"><strong>{{competition.group_name}}</strong></h6>
+            <div v-if="competition.matchCount == 0">
+                {{$lang.pitch_planner_no_games}}
+            </div>
 
-          <div v-if="competition.matchCount == 0">
-              {{$lang.pitch_planner_no_games}}
-          </div>
-          <div class="text-center mt-3"
-          v-if="match.isScheduled!=1"
-          v-for="match in competition.matchList"
-          :data-text="match.matchName">
-              <draggable-match-event :match="match"></draggable-match-event>
+            <div class="text-center mt-3"
+            v-if="match.isScheduled!=1"
+            v-for="match in competition.matchList"
+            :data-text="match.matchName">
+                <draggable-match-event :match="match"></draggable-match-event>
+            </div>
           </div>
         </div>
         <br>
