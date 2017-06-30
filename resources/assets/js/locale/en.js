aff13946d8634cdc5703341845bd8c75fbc1252b
rishabh <rishabh>
2017-06-30 12:43:15 +0530

    Ticket #65

--- resources/assets/js/locale/en.js
+++ resources/assets/js/locale/en.js
@@ -365,6 +365,7 @@ module.exports = {
   summary_label_message: "Messages",
 
   summary_schedule_matches_time: "Time",
+  summary_schedule_date_time: "Date and time",
   summary_schedule_matches_draw: "Draw",
   summary_schedule_matches_team: "Team",
   summary_schedule_matches_score: "Score",
