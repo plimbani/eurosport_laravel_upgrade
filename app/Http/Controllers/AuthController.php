5c6cf3c881db3544e170b2c145d0bf6f116f4946
Kamal Nayak <knayak@aecordigital.com>
2017-07-20 15:44:13 +0530

    Change response message for wrong credential

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'Invalid Credentials.'], 401);
+                return response()->json(['error' => 'The login details entered are invalid. If you have forgotten your password please follow the forgot password process.'], 401);
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
