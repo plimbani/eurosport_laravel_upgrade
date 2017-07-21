7c353b3bdf7ce337013572b9ddb13cee3909ddbf
rishabh <rishabh>
2017-07-21 17:59:10 +0530

    chages done

--- resources/views/pitchplanner/pitch.blade.php
+++ resources/views/pitchplanner/pitch.blade.php
@@ -36,32 +36,32 @@
     <h4>Match Details</h4>
   <thead>
         <tr>
-            <td align="left">Match number</td>
+            <td align="left">&nbsp;Match number</td>
             <td>&nbsp;{{$data['match_number']}}</td>
         </tr>
         <tr>
-           <td align="left">Date</td>
+           <td align="left">&nbsp;Date</td>
            <td>&nbsp;{{ Carbon\Carbon::parse( $data['match_datetime'])->format('H:i D d M Y') }}</td>
         </tr>
         <tr>
-             <td align="left">Pitch</td>
+             <td align="left">&nbsp;Pitch</td>
              <td>&nbsp;{{ $data['pitch']['pitch_number']}}</td>
         </tr> 
         <tr>
-             <td align="left">Referee</td>
-            @if($data['referee']['last_name'] && $data['referee']['first_name'])
-              <td align="left">{{ $data['referee']['last_name']}},{{ $data['referee']['first_name']}}</td>
+             <td align="left">&nbsp;Referee</td>
+              @if($data['referee']['last_name'] && $data['referee']['first_name'])
+              <td>{{ $data['referee']['last_name']}},{{ $data['referee']['first_name']}}</td>
             @else
             <td align="left"></td>
             @endif
         </tr>
         <tr>
-            <td align="left">Result</td>
-             <td>&nbsp; {{$data['home_team_name']}} - {{$data['hometeam_score']}}<br>
-                 &nbsp; {{$data['away_team_name']}} - {{$data['awayteam_score']}}</td>
+            <td align="left">&nbsp;Result</td>
+             <td>&nbsp;{{$data['home_team_name']}} - {{$data['hometeam_score']}}<br>
+                 &nbsp;{{$data['away_team_name']}} - {{$data['awayteam_score']}}</td>
         </tr> 
         <tr>
-            <td align="left">Status</td>
+            <td align="left">&nbsp;Status</td>
             @if($data['match_status'] == '0')
             <td align="left"></td>
             @else
@@ -69,11 +69,11 @@
             @endif
         </tr>
         <tr>
-          <td align="left">Winner</td>
+          <td align="left">&nbsp;Winner</td>
           <td>&nbsp;{{ $data['name']}}</td>
         </tr>
         <tr>
-          <td align="left">Comments</td>
+          <td align="left">&nbsp;Comments</td>
           <td>&nbsp;{{ $data['comments']}}</td>
         </tr>  
     </thead>
