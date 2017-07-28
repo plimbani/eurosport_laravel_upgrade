7ed95c40bf07317d0a6dae7fcb94b547787dc7da
Kamal Nayak <knayak@aecordigital.com>
2017-07-28 12:11:09 +0530

    resolve issue for pitch unschedule

--- app/Api/Services/PitchService.php
+++ app/Api/Services/PitchService.php
@@ -61,11 +61,12 @@ class PitchService implements PitchContract
 
         // dd($dataArr);
         $pitchdata = $this->pitchRepoObj->edit($dataArr,$pitchId);
-        $this->unScheduleAllocatedMatch($dataArr,$pitchId);
+
         if($pitchdata){
-            $this->unScheduleAllocatedMatch($dataArr,$pitchId);
             $this->pitchAvailableRepoObj->removePitchAvailability($pitchId);
             $data1 = $this->pitchAvailableRepoObj->createPitch($dataArr, $pitchId);
+
+            $this->unScheduleAllocatedMatch($dataArr,$pitchId);
         }
         if ($data) {
             return ['status_code' => '200', 'message' => 'Data Successfully Updated'];
@@ -139,15 +140,19 @@ class PitchService implements PitchContract
 
        $stage_start_date_time = $stage->stage_start_date.' '.$stage->stage_start_time;
        $stage_end_date_time = $stage->stage_end_date.' '.$stage->stage_end_time;
-       if($stage->break_enable == 1) {
+       if($stage->break_enable == true) {
+
                   $stage_break_start_date_time = $stage->stage_continue_date.' '.$stage->break_start_time;
                   $stage_break_end_date_time = $stage->stage_continue_date.' '.$stage->break_end_time;
+
+             $stage_break_start_date_time =date("Y-m-d H:i:s",strtotime($stage_break_start_date_time));
+             $stage_break_end_date_time =date("Y-m-d H:i:s",strtotime($stage_break_end_date_time));
         }
 
           $stage_start_date_time =  date("Y-m-d H:i:s",strtotime($stage_start_date_time));
           $stage_end_date_time =  date("Y-m-d H:i:s",strtotime($stage_end_date_time));
       foreach ($matches as $matchKey=>$match) {
-
+          //echo '2';exit;
                 $matchStartDateTime  = $match->match_datetime;
                 $matchEndDateTime  = $match->match_endtime;
                 /*echo '<br>StageKey:'.$Stagekey;
@@ -156,16 +161,34 @@ class PitchService implements PitchContract
                 echo '<br>StageStartEndTime:'.$stage_end_date_time;
                 echo '<br>MatchStartDateTime:'.$matchStartDateTime;*/
 
-                echo 'stagedate:'.$stageDate = $stage->stage_start_date;
-                echo 'mtchdate:'.$matchDate = date("Y-m-d",strtotime($match->match_datetime));
+                echo 'stagedate:'.$stageDate = date("Y-m-d",strtotime($stage->stage_start_date));
+                echo '<br>mtchdate:'.$matchDate = date("Y-m-d",strtotime($match->match_datetime));
 
-
+                echo '<br>matchDateTime'.$matchStartDateTime;
                 // Must have Same Date
-                if(date('Y-m-d',$stage_start_date_time) == date('Y-m-d',$matchStartDateTime)) {
-                  if($this->check_date_is_within_range( $stage_start_date_time,$stage_end_date_time,$matchStartDateTime)){
-                      //echo '<br>IN RANGE:'.$match->id;
-                    } else {
-                      echo '<br>OutherRange:'.$match->id;
+                if($stageDate== $matchDate) {
+                  echo '<br>Same';
+                  if(!$this->check_date_is_within_range( $stage_start_date_time,$stage_end_date_time,$matchStartDateTime)){
+                      echo '<br>Outer RANGE:'.$match->id;
+                      $update_match = DB::table('temp_fixtures')
+                        ->where('temp_fixtures.id','=',$match->id)
+                        ->update(['is_scheduled' => 0]);
+                    }
+                    // Also check for match fall into break
+                    if($stage->break_enable == 1) {
+                      echo '<br>breakStartTime'.$stage_break_start_date_time;
+                      echo '<br>breakEndtime'.$stage_break_end_date_time;
+                      echo '<br>matchtime'.$matchStartDateTime;
+
+                      // if it is in that range then unschedule it
+                      if($this->check_date_is_within_range( $stage_break_start_date_time,$stage_break_end_date_time,$matchStartDateTime)){
+                      echo '<br>Outer RANGE break:'.$match->id;
+                      $update_match = DB::table('temp_fixtures')
+                        ->where('temp_fixtures.id','=',$match->id)
+                        ->update(['is_scheduled' => 0]);
+                     } else {
+                      echo '<br>not brk';
+                     }
                     }
                 }
 
@@ -183,6 +206,7 @@ class PitchService implements PitchContract
                 } */
             }
         }
+       // exit;
     }
     private function check_date_is_within_range($start_date, $end_date, $todays_date)
     {
