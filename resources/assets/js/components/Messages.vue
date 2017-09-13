1f3700fe0b64e9cf46e32aa58f359341cc39bf90
rishabh <rishabh>
2017-09-13 20:19:10 +0530

    changes done

--- resources/assets/js/components/Messages.vue
+++ resources/assets/js/components/Messages.vue
@@ -5,19 +5,8 @@
 
 	    <div class="card">
 	      <div class="card-block">
-	        <div class="row align-items-center">
-	            <div class="col-3">
-	               <h6 class="mt-3"><strong>{{$lang.summary_table_text}}</strong></h6>
-	            </div>
-	        	<div class="col-9">
-                    <button type="button" class="btn btn-primary pull-right"
-                       @click="addMessage()"><small><i class="jv-icon jv-plus"></i>
-                       </small>&nbsp;{{$lang.summary_message_button}}
-                    </button>
-                </div>
-           	</div>   
+	          <h6 class="mt4"><strong>{{$lang.summary_table_text}}</strong></h6>
 	          <messageHistoryList></messageHistoryList>
-	          <AddMessageModel v-if="messageStatus"></AddMessageModel>
 	      </div>
 	    </div>
 
@@ -26,31 +15,9 @@
 </template>
 <script type="text/babel">
 import MessageHistoryList from './MessageHistoryList.vue'
-import AddMessageModel from './AddMessageModel.vue'
 export default {
-
-	data() {
-		return {
-			messageStatus: false
-		}
-	},
-
 components: {
-      MessageHistoryList, AddMessageModel
-  },
-
-  methods: {
-      addMessage() {
-        let vm =this
-        this.messageStatus = true
-        this.type='add'
-        setTimeout(function(){
-          $('#exampleModal').modal('show')
-            $("#exampleModal").on('hidden.bs.modal', function () {
-              vm.messageStatus = false
-          });
-        },500)
-    }
-    }
+      MessageHistoryList
+  }
 }
 </script>
