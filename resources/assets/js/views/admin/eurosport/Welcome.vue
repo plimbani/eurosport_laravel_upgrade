79a2e05d2049c6d623d139dd40e73c22c5cbb380
krunal <kparikh@aecordigital.com>
2017-09-15 14:22:21 +0530

    Add changes

--- resources/assets/js/views/admin/eurosport/Welcome.vue
+++ resources/assets/js/views/admin/eurosport/Welcome.vue
@@ -3,11 +3,11 @@
     <!-- <div class="row">
       <div class="col-md-12">
         <div class="alert alert-info alert-dismissible" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           Hi Chris, Welcome to Tournament Planner. Got a question? Check the <strong> Help </strong>page or <strong> contact us </strong>for assistance.
 
         </div>
-      </div>  
+      </div>
     </div> -->
     <div class="row home-content">
       <div class="col-sm-6">
@@ -15,14 +15,14 @@
           <div class="card-header">
             <h5 class="text-center"><strong>{{$lang.welcome_manage_tournament}}</strong></h5>
           </div>
-          <div class="card-block text-center">  
+          <div class="card-block text-center">
             <div class= "form-group">
-              <button class="btn btn-primary col-sm-8 btn-theme" 
+              <button class="btn btn-primary col-sm-8 btn-theme"
               @click="addNewTournament()" v-if="(userDetails.role_name != 'Tournament administrator' &&  userDetails.role_name != 'Internal administrator')">
               {{$lang.welcome_add_button_new_edition}}</button>
-            </div>          
+            </div>
               <div class="form-group">
-                <tournamentDropDown></tournamentDropDown>              
+                <tournamentDropDown></tournamentDropDown>
               </div>
           </div>
         </div>
@@ -30,22 +30,22 @@
       <div class="col-sm-6">
       <div class="card">
         <div class="card-header">
-          <h5 class="text-center" 
-          v-if="(userDetails.role_name != 'Tournament administrator' &&  userDetails.role_name != 'Internal administrator')"><strong>{{$lang.welcome_add_new_tournament}}</strong></h5>
+          <h5 class="text-center"
+          v-if="(userDetails.role_name != 'Tournament administrator' &&  userDetails.role_name != 'Internal administrator')"><strong>{{$lang.welcome_add_new_user}}</strong></h5>
           <h5 class="text-center" v-else><strong>{{$lang.welcome_add_tournament}}</strong>
           </h5>
         </div>
-        <div class="card-block text-center">          
+        <div class="card-block text-center">
             <div class="form-group" v-if="(userDetails.role_name == 'Internal administrator' || userDetails.role_name == 'Tournament administrator') ">
               <ol class="col-sm-8 offset-sm-2">
                 <li class="text-left">{{$lang.welcome_add_new_tournament_edition_details}}</li>
                 <li class="text-left">{{$lang.welcome_add_new_tournament_review}}</li>
                 <li class="text-left">{{$lang.welcome_add_new_tournament_publish}}!</li>
-              </ol> 
+              </ol>
             </div>
             <button class="btn btn-primary col-sm-8 btn-theme" @click="addNewTournament()" v-if="(userDetails.role_name == 'Tournament administrator' || userDetails.role_name == 'Internal administrator') ">{{$lang.welcome_add_user}} </button>
             <button class="btn btn-primary col-sm-8 btn-theme" @click="userList()" v-else>{{$lang.welcome_add_new_user}}</button>
-            <br>          
+            <br>
         </div>
       </div>
       </div>
@@ -55,7 +55,7 @@
 <script>
 import TournamentDropDown from '../../../components/TournamentDropDown.vue'
 import Ls from '../../../services/ls'
-export default {    
+export default {
   components : {
     TournamentDropDown
   },
@@ -63,33 +63,33 @@ export default {
 computed: {
     userDetails: function() {
       return this.$store.state.Users.userDetails
-    },    
+    },
   },
   mounted() {
-    
-    let tournamentAdd  = {name:'', 'currentPage':'Home'}  
+
+    let tournamentAdd  = {name:'', 'currentPage':'Home'}
     this.$store.dispatch('SetTournamentName', tournamentAdd)
 
     // Here we set Default Value For Tournament
-    
+
     /*let userDetails = this.$store.state.Users.userDetails
         // this.userDetails = this.$store.state.Users.userDetails
-   
+
     let that = this
      setTimeout(function(){
     //   // console.log(userDetails.length,'hh')
        if(userDetails.length == 0)
        {
-        
+
          let email = Ls.get('email');
-          
+
          // Now here we are call and fetch user details
          let userData = {'email':email}
          that.$store.dispatch('getUserDetails', userData);
-          that.userDetails = that.$store.state.Users.userDetails          
+          that.userDetails = that.$store.state.Users.userDetails
        }
-        let tournamentAdd  = {name:'', 'currentPage':'Home'}  
-             
+        let tournamentAdd  = {name:'', 'currentPage':'Home'}
+
        that.$store.dispatch('SetTournamentName', tournamentAdd)
      },1000)
 
@@ -97,25 +97,25 @@ computed: {
      */
   },
   methods : {
-    addNewTournament() {     
-      // we have to Dispatch there is New Tournament 
-      let tournamentAdd  = {name:'Your Tournament', 
-      'currentPage':'TournamentAdd','tournamentStartDate': '','tournamentEndDate':'','tournamentDays': 0,'tournamentStatus': '','id': ''}        
-      let currentNavigationData = {activeTab:'tournament_add', currentPage: 
+    addNewTournament() {
+      // we have to Dispatch there is New Tournament
+      let tournamentAdd  = {name:'Your Tournament',
+      'currentPage':'TournamentAdd','tournamentStartDate': '','tournamentEndDate':'','tournamentDays': 0,'tournamentStatus': '','id': ''}
+      let currentNavigationData = {activeTab:'tournament_add', currentPage:
       'Add Tournament'}
       this.$store.dispatch('setActiveTab', currentNavigationData)
 
-      this.$store.dispatch('SetTournamentName', tournamentAdd)      
-      this.$router.push({name: 'tournament_add'})      
+      this.$store.dispatch('SetTournamentName', tournamentAdd)
+      this.$router.push({name: 'tournament_add'})
     },
-    userList() {      
-      let currentNavigationData = {activeTab:'tournament_add', currentPage: 
+    userList() {
+      let currentNavigationData = {activeTab:'tournament_add', currentPage:
       'Users'}
       this.$store.dispatch('setActiveTab', currentNavigationData)
 
-      let tournamentAdd  = {name:'', 'currentPage':'Users'}        
+      let tournamentAdd  = {name:'', 'currentPage':'Users'}
       this.$store.dispatch('SetTournamentName', tournamentAdd)
-      
+
       this.$router.push({ name: 'users_list', params: {registerType:'desktop'}})
     }
   }
