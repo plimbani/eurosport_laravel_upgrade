3a49186749d2431ff08e0565e3b5f57879a17fa4
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-09-23 17:54:09 +0530

    changes

--- resources/views/automate_tournament/competetions.blade.php
+++ resources/views/automate_tournament/competetions.blade.php
@@ -16,7 +16,7 @@
           <div class="text-danger"><strong>Note: On click of "Schedule Match & Insert Result", it will unschedule all the matches of the tournament AND then it will schedule and insert match result for only of the selected competetion</strong></div>
           <div class="row">
               <div class="col-md-12">
-                <h3><u>Competetions</u></h3>
+                <h3><u>Competitions</u></h3>
               </div>
               @foreach($tournamentCompetationTemplates as $tct)
                 <div class="col-md-12">
