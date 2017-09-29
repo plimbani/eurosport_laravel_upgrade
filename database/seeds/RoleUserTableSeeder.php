1e25bf7de8a79b02963817c7f40567e41810b5a6
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-09-29 14:18:31 +0530

    seed changes

--- database/seeds/RoleUserTableSeeder.php
+++ database/seeds/RoleUserTableSeeder.php
@@ -13,16 +13,11 @@ class RoleUserTableSeeder extends Seeder
     public function run()
     {
         DB::table('role_user')->truncate();
-        $role = DB::table('roles')->take(3)->get()->toArray();
-        $users = DB::table('users')->take(3)->select('id')->get()->toArray();
 
         DB::table('role_user')->insert([
         	['role_id' => 1, 'user_id' => 1, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-        	['role_id' => 2, 'user_id' => 2, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-        	['role_id' => 3, 'user_id' => 3, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['role_id' => 4, 'user_id' => 4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-          ['role_id' => 5, 'user_id' => 5, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-
+        	['role_id' => 1, 'user_id' => 2, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+        	['role_id' => 1, 'user_id' => 3, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
         ]);
     }
 }
