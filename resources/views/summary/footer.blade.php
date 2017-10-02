b31c5e668b6d656e9a2f80fd61c5fbe1c2114fd0
rishabh <rishabh>
2017-10-02 18:28:38 +0530

    Ticket 186

---
+++ resources/views/summary/footer.blade.php
@@ -0,0 +1,7 @@
+<table width="100%">
+ <tr>
+  <td width="33%"></td>
+  <td width="33%" align="center"><img  src="{{ asset('assets/img/logo-desk.svg')}}" id="logo-desk" alt="Laraspace Logo" class="hidden-sm-down" width="100px" height="50px"></td>
+  <td width="33%" align="right">Page {{ $query['page'] }} of {{ $query['topage'] }}</td>
+ </tr>
+</table>
