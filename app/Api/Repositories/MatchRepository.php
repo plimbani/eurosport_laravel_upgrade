f1c73a5532731c843ed0985faebb81c265cd6103
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-11-06 18:45:09 +0530

    changes

--- app/Api/Repositories/MatchRepository.php
+++ app/Api/Repositories/MatchRepository.php
@@ -152,25 +152,6 @@ class MatchRepository
 
     }
 
-            // ->leftjoin('match_results', 'temp_fixtures.match_result_id', '=', 'match_results.id')
-            // ->leftjoin('referee', 'referee.id', '=', 'temp_fixtures.referee_id')
-            // ->groupBy('temp_fixtures.id')
-            // ->select('temp_fixtures.id as fid','temp_fixtures.match_number as match_number' ,'competitions.competation_type as round' ,'competitions.name as competation_name','competitions.color_code as competation_color_code' , 'competitions.team_size as team_size','temp_fixtures.match_datetime','temp_fixtures.match_endtime','temp_fixtures.match_status','temp_fixtures.age_group_id','temp_fixtures.match_winner',
-            //   'match_winner.name as MatchWinner',
-            //     'venues.id as venueId', 'competitions.id as competitionId',
-            //     'venues.venue_coordinates as venueCoordinates',
-            //     'pitches.type as pitchType','venues.address1 as venueaddress',
-            //     'venues.state as venueState','venues.county as venueCounty',
-            //     'venues.city as venueCity','venues.country as venueCountry',
-            //     'venues.postcode as venuePostcode',
-            //     'tournament_competation_template.group_name as group_name',
-            //     'tournament_competation_template.category_age_color as category_age_color','venues.name as venue_name','pitches.pitch_number','referee.first_name as referee_name','temp_fixtures.referee_id as referee_id','referee.first_name as first_name','referee.last_name as last_name','home_team.name as HomeTeam','away_team.name as AwayTeam',
-            //     'temp_fixtures.home_team as Home_id','temp_fixtures.away_team as Away_id',
-            //     DB::raw('CONCAT("'.$this->getAWSUrl.'", HomeFlag.logo) AS HomeFlagLogo'),
-            //     DB::raw('CONCAT("'.$this->getAWSUrl.'", AwayFlag.logo) AS AwayFlagLogo'),
-            //     'HomeFlag.country_flag as HomeCountryFlag',
-            //     'AwayFlag.country_flag as AwayCountryFlag',
-
     public function checkTeamIntervalForMatchesOnCategoryUpdate($matchData) {
       $matches = [];
       $matches = DB::table('temp_fixtures')
