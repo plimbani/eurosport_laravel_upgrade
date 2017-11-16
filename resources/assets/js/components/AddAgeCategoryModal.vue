974ec8009df65a73e6a192ee791042da0741bd55
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-11-16 18:47:03 +0530

    changes

--- resources/assets/js/components/AddAgeCategoryModal.vue
+++ resources/assets/js/components/AddAgeCategoryModal.vue
@@ -108,19 +108,15 @@
             <div class="col-sm-8">
               <div class="row align-items-center">
                 <div class="col-sm-2">
-                  <select class="form-control ls-select2" name="duration_selected" v-model="duration_selected">
-                        <option value="1">1 x</option>
-                        <option value="2">2 x</option>
+                  <select class="form-control ls-select2" name="halves_RR" v-model="competation_format.halves_RR" @change="showHideHalfTimeBreakRR()">
+                    <option value="1">1 x</option>
+                    <option value="2">2 x</option>
                   </select>
                 </div>
                 <div class="col-sm-4">
                 <select class="form-control ls-select2 " v-model="competation_format.game_duration_RR" @change="updateMatchTime()">
                 <option v-for="(item,key) in game_duration_rr_array[0]"
                  v-bind:value="item">{{key}}</option>
-                    <!--<option value="20">10</option>
-                    <option value="30">15</option>
-                    <option value="40">20</option>
-                    <option value="other">Other</option>-->
                 </select>
                 </div>
                 <span v-if="competation_format.game_duration_RR
@@ -137,7 +133,7 @@
             <div class="col-sm-8">
               <div class="row align-items-center">
                  <div class="col-sm-2">
-                    <select id="duration" name="duration_final" v-model="duration_final" class="form-control ls-select2">
+                    <select id="duration" name="halves_FM" v-model="competation_format.halves_FM" class="form-control ls-select2" @change="showHideHalfTimeBreakFM()">
                           <option value="1">1 x</option>
                           <option value="2">2 x</option>
                     </select>
@@ -156,7 +152,7 @@
               </div>
             </div>
           </div>
-          <div class="form-group row align-items-center" v-if="duration_selected == 2">
+          <div class="form-group row align-items-center" v-show="haveTwoHalvesRR">
             <div class="col-sm-4 form-control-label">{{$lang.competation_modal_half_time_break}}</div>
             <div class="col-sm-8">
               <div class="row">
@@ -169,7 +165,7 @@
                <span class="help is-danger" v-show="errors.has('half_time_break')">{{$lang.competation_modal_half_time_break_required}}</span>
             </div>
           </div>
-          <div class="form-group row align-items-center" v-if="duration_final == 2">
+          <div class="form-group row align-items-center" v-show="haveTwoHalvesFM">
             <div class="col-sm-4 form-control-label">{{$lang.competation_modal_half_time_break_final}}</div>
             <div class="col-sm-8">
              <div class="row">
@@ -339,9 +335,9 @@ export default {
       isAgeCategoryDisabled: false,
       exceedTeamLimit: false,
       exceedTeamLimitMessage: '',
-      duration_selected: 2,
-      duration_final: 2,
       isSaveInProcess: false,
+      haveTwoHalvesRR: true,
+      haveTwoHalvesFM: true,
       categoryAgeArr: ['U08/5','U09','U09/5','U09/7','U10','U10/5','U10/7','U10/9','U10/5A','U10/7A','U10/5B','U10/7B','U11','U11/11','U11/7','U11/7A','U11/7B','U12','U12/7','U12/8','U12/9','U12-A','U12/7A','U12/8A','U12-B','U12/7B','U12/8B','U13','U13/7','U13/8','U13/9','U13-A','U13/7A','U13/8A','U13/9A','U13-B','U13/8B','U13/9B','U14','U14/7','U14-A','U14-B','U15','U15/7','U15-A','U15-B','U16','U16-A','U16-B','U17','U17-A','U17-B','U18','U19','U19-A','U19-B','U10-U9','G08/5','G09/5','G09/7','G10/5','G10/7','G10/7A','G10/7B','G11','G11/7','G12','G12/7','G12/8','G12/9','G12/7A','G12/7B','G13','G13/7','G13/8','G13/9','G13/7A','G13/7B','G14','G14/7','G14/8','G14-A','G14-B','G15','G15/7','G15/8','G15-A','G15-B','G16','G17','G17/7','G17-A','G17-B','G18','G18/7','G18-A','G18-B','G19','G19-A','G19-B','M-O','M-O/5','M-O/7','M32','M35','M35/7','W-O','W-O/7'],
       categoryAgeColorArr: {
         'U08/5' : '#ffc0cb','U09' : '#008080','U09/5' : '#ffe4e1','U09/7' : '#ff0000','U10' : '#ffd700','U10/5' : '#d3ffce','U10/7' : '#00ffff','U10/9' : '#40e0d0','U10/5A' : '#ff7373','U10/7A' : '#e6e6fa','U10/5B' : '#0000ff','U10/7B' : '#ffa500','U11' : '#b0e0e6','U11/11' : '#7fffd4','U11/7' : '#333333','U11/7A' : '#faebd7','U11/7B' : '#003366','U12' : '#fa8072','U12/7' : '#800080','U12/8' : '#20b2aa','U12/9' : '#ffb6c1','U12-A' : '#c6e2ff','U12/7A' : '#00ff00','U12/8A' : '#f6546a','U12-B' : '#f08080','U12/7B' : '#468499','U12/8B' : '#ffff00','U13' : '#ffc3a0','U13/7' : '#088da5','U13/8' : '#fff68f','U13/9' : '#ff6666','U13-A' : '#00ced1','U13/7A' : '#66cdaa','U13/8A' : '#800000','U13/9A' : '#660066','U13-B' : '#ff00ff','U13/8B' : '#D8BFD8','U13/9B' : '#c39797','U14' : '#c0d6e4','U14/7' : '#0e2f44','U14-A' : '#cbbeb5','U14-B' : '#ff7f50','U15' : '#ffdab9','U15/7' : '#990000','U15-A' : '#daa520','U15-B' : '#8b0000','U16' : '#b4eeb4','U16-A' : '#afeeee','U16-B' : '#ffff66','U17' : '#f5f5dc','U17-A' : '#81d8d0','U17-B' : '#b6fcd5','U18' : '#66cccc','U19' : '#00ff7f','U19-A' : '#ccff00','U19-B' : '#cc0000','U10-U9' : '#a0db8e','G08/5' : '#8a2be2','G09/5' : '#ff4040','G09/7' : '#3399ff','G10/5' : '#3b5998','G10/7' : '#0099cc','G10/7A' : '#794044','G10/7B' : '#ff4444','G11' : '#000080','G11/7' : '#6897bb','G12' : '#6dc066','G12/7' : '#31698a','G12/8' : '#191970','G12/9' : '#191919','G12/7A' : '#4169e1','G12/7B' : '#B0171F','G13' : '#FFBBFF','G13/7' : '#7D26CD','G13/8' : '#27408B','G13/9' : '#00C78C','G13/7A' : '#3D9140','G13/7B' : '#00EE00','G14' : '#EEEE00','G14/7' : '#FF9912','G14/8' : '#CD6600','G14-A' : '#F4A460','G14-B' : '#8B4C39','G15' : '#CD0000','G15/7' : '#8E8E38','G15/8' : '#FFEC8B','G15-A' : '#EE9A49','G15-B' : '#CD8162','G16' : '#BBFFFF','G17' : '#008B8B','G17/7' : '#1874CD','G17-A' : '#9F79EE','G17-B' : '#EE3A8C','G18' : '#92C685','G18/7' : '#C2B182','G18-A' : '#47CE6E','G18-B' : '#00A998','G19' : '#C2A9FD','G19-A' : '#D5FD30','G19-B' : '#CACA8E','M-O' : '#8D8812','M-O/5' : '#0075EA','M-O/7' : '#DCB8D4','M32' : '#F0FF18','M35' : '#60262E','M35/7' : '#B2F3B7','W-O' : '#532C5E','W-O/7' : '#BBF47F'
@@ -419,15 +415,15 @@ export default {
 
     });
     this.game_duration_rr_array.push ({
-      '10':'20',
-      '15':'30',
-      '20':'40',
+      '10':'10',
+      '15':'15',
+      '20':'20',
       'Other':'other'
     });
      this.game_duration_fm_array.push ({
-      '10':'20',
-      '15':'30',
-      '20':'40',
+      '10':'10',
+      '15':'15',
+      '20':'20',
       'Other':'other'
     });
     this.match_interval_rr_array.push ({
@@ -474,11 +470,7 @@ export default {
     },
     initialState() {
       return {
-         ageCategory_name:'',category_age:'',category_age_color:null,game_duration_RR:'20',game_duration_FM:'20',
-        halftime_break_RR:'5',halftime_break_FM:'5',match_interval_RR:'5',match_interval_FM:'5',tournamentTemplate:[],
-        tournament_id: '', competation_format_id:'0',id:'',
-        nwTemplate:[],game_duration_RR_other:'20',
-      game_duration_FM_other:'20',match_interval_RR_other:'20',match_interval_FM_other:'20',min_matches:'',team_interval:'40'
+          ageCategory_name:'',category_age:'',category_age_color:null,game_duration_RR:'20',halves_RR:'2',game_duration_FM:'20',halves_FM:'2',halftime_break_RR:'5',halftime_break_FM:'5',match_interval_RR:'5',match_interval_FM:'5',tournamentTemplate:[],tournament_id: '', competation_format_id:'0',id:'',nwTemplate:[],game_duration_RR_other:'20',game_duration_FM_other:'20',match_interval_RR_other:'20',match_interval_FM_other:'20',min_matches:'',team_interval:'40'
       }
     },
     setEdit(id) {
@@ -687,9 +679,29 @@ export default {
     onTouch () {
       this.isTouched = true
     },
+    showHideHalfTimeBreakRR() {
+      if(this.competation_format.halves_RR == 2) {
+        this.haveTwoHalvesRR = true;
+        this.competation_format.halftime_break_RR = '5';
+      } else {
+        this.haveTwoHalvesRR = false;
+        this.competation_format.halftime_break_RR = '0';
+      }
+    },
+    showHideHalfTimeBreakFM() {
+      if(this.competation_format.halves_FM == 2) {
+        this.haveTwoHalvesFM = true;
+        this.competation_format.halftime_break_FM = '5';
+      } else {
+        this.haveTwoHalvesFM = false;
+        this.competation_format.halftime_break_FM = '0';
+      }
+    },
     updateMatchTime: _.debounce(function (e) {
+      var halves_RR = this.competation_format.halves_RR;
       var game_duration_RR = this.competation_format.game_duration_RR;
       var game_duration_RR_other = this.competation_format.game_duration_RR_other;
+      var halves_FM = this.competation_format.halves_FM;
       var game_duration_FM = this.competation_format.game_duration_FM;
       var game_duration_FM_other= this.competation_format.game_duration_FM_other;
       var halftime_break_RR = this.competation_format.halftime_break_RR;
@@ -700,10 +712,10 @@ export default {
       var match_interval_FM_other= this.competation_format.match_interval_FM_other;
 
       if(game_duration_RR == 'other') {
-        game_duration_RR = 2 * game_duration_RR_other;
+        game_duration_RR = halves_RR * game_duration_RR_other;
       }
       if(game_duration_FM == 'other') {
-        game_duration_FM = 2 * game_duration_FM_other;
+        game_duration_FM = halves_FM * game_duration_FM_other;
       }
       if(match_interval_RR == 'other') {
         match_interval_RR = match_interval_RR_other;
