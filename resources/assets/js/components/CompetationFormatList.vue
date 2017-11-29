945e8c1a2d2dc428893d4dd59cdacb41d4169278
Sunny Sheth <ssheth@aecordigital.com>
2017-11-29 12:14:14 +0530

    Ticket #262 changes

--- resources/assets/js/components/CompetationFormatList.vue
+++ resources/assets/js/components/CompetationFormatList.vue
@@ -46,6 +46,30 @@
           <AddAgeCateogryModel v-if="categoryStatus"></AddAgeCateogryModel>
           <delete-modal :deleteConfirmMsg="deleteConfirmMsg" @confirmed="deleteConfirmed()"></delete-modal>
           <competationModal :templateData="templateData" :totalTime="totalTime"></competationModal>
+            <div class="modal fade p-0" id="large-image-modal" tabindex="-1" role="dialog" aria-labelledby="large-image-modal" aria-hidden="true">
+              <div class="modal-dialog modal-full" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="d-flex align-items-center justify-content-centers">
+                      <div class="d-block mx-auto">
+                        <img src="https://image.prntscr.com/image/2pYwsqIZS7mv59HxwI8HKA.jpg">
+                        <!-- <img src="//placehold.it/250x250"> -->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div> -->
+                </div>
+              </div>
+            </div>
         </table>
       </div>
     </div>
