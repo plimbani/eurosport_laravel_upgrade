fc813530a59aba24bdaa029b5eeb11c7f76c9959
Sunny Sheth <ssheth@aecordigital.com>
2017-11-30 11:33:20 +0530

    Ticket #262 changes

---
+++ database/migrations/2017_11_29_072738_add_image_to_tournament_template_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageToTournamentTemplateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tournament_template', function($table) {
+           $table->string('image', 100)->nullable()->after('json_data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tournament_template', function($table) {
+            $table->dropColumn('image');
+        });
+    }
+}
