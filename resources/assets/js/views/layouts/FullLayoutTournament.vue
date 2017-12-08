fdeeb397e25fb76ed01b899f5967264c5ee8332c
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-12-08 19:24:32 +0530

    changes

---
+++ resources/assets/js/views/layouts/FullLayoutTournament.vue
@@ -0,0 +1,41 @@
+<template class="template-container">
+    <div class="modal fade p-0" id="enlarge_pitch_planner_modal" tabindex="-1" role="dialog" aria-labelledby="enlarge_pitch_planner_modal" aria-hidden="true">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Pitch planner</h5>
+                <button type="button" class="close" aria-label="Close" @click="showPitchPlanner()">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <router-view></router-view>
+            </div>  
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+
+    export default {
+        mounted() {
+            let vm = this;
+            $('#enlarge_pitch_planner_modal').modal('show')
+            // $("#enlarge_pitch_planner_modal").one('hidden.bs.modal', function (event) {
+            //     event.stopPropagation();
+            //     vm.$router.push({name: 'pitch_planner'})
+            // });
+        },
+        data() {
+            return {
+
+            }
+        },
+        components : {
+        },
+        method: {
+            showPitchPlanner() {
+                vm.$router.push({name: 'pitch_planner'});
+            }
+        },
+    }
+</script>
