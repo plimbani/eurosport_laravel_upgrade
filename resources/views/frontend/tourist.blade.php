22dee74d0ecb2f6976e0658d9484c07db613bffb
rishabh <rishabh>
2018-02-26 14:50:26 +0530

    ticket 350 changes

--- resources/views/frontend/tourist.blade.php
+++ resources/views/frontend/tourist.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
 
+<h1>Tourist information</h1>
+
+{!! $touristContent->content !!}
+
 @endsection
--- resources/views/frontend/visitor.blade.php
+++ resources/views/frontend/visitor.blade.php
@@ -2,4 +2,22 @@
 
 @section('content')
 
+<h1>Visitors</h1>
+
+@if($arrivalCheckInInformation != '')
+<h2>Arrival and check-in </h2>
+@endif
+
+{!! $arrivalCheckInInformation !!}
+
+@if($publicTransport != '')
+<h2>Public transport</h2>
+{!! $publicTransport !!}
+@endif
+
+@if($tips != '')
+<h2>Tips for visitors</h2>
+{!! $tips !!}
+@endif
+
 @endsection
