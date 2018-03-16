a4b81328bb3082f76fe978ee9aaeed240c80ab98
Usama <upatel@aecordigital.com>
2018-03-16 16:28:35 +0530

    Trait updated

--- app/Traits/TournamentAccess.php
+++ app/Traits/TournamentAccess.php
@@ -26,4 +26,25 @@ trait TournamentAccess
 		}
 		return true;
 	}
+
+	/*
+	 * Check for tournament access to user
+	 *
+	 * @return response
+	 */
+	protected function checkForWritePermissionByTournament($id)
+	{
+		$user = $this->getCurrentLoggedInUserDetail();
+		if($user->hasRole('tournament.administrator')) {
+			$tournamentsIds = $user->tournaments()->pluck('id')->toArray();
+			if (in_array($id, $tournamentsIds)) {
+				return true;
+			}
+			return false;
+		}
+		if($user->hasRole('mobile.user')) {
+			return false;
+		}
+		return true;
+	}
 }
