c8851e7292397de1f7d47b78adfb1dcfadcaaaf0
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-03-20 16:47:01 +0530

    frontend changes

--- resources/views/partials/frontend/meta.blade.php
+++ resources/views/partials/frontend/meta.blade.php
@@ -17,19 +17,20 @@
 <meta name="msapplication-navbutton-color" content="#667eea">
 
 <!-- Open Graph Meta -->
-<meta property="og:title" content="{{ $meta_details['tournament_name'] }} - {{ $pageTitle }}">
-<meta property="og:site_name" content="{{ $meta_details['tournament_name'] }}">
-<meta property="og:description" content="{{ $meta_details['description'] }}">
-<meta property="og:type" content="website">
-<meta property="og:url" content="{{ url()->current() }}">
-<meta property="og:image" content="{{ $meta_details['open_graph_image'] }}">
+<meta property="og:title" content="{{ $meta_details['tournament_name'] }} - {{ $pageTitle }}" />
+<meta property="og:site_name" content="{{ $meta_details['tournament_name'] }}" />
+<meta property="og:description" content="{{ $meta_details['description'] }}" />
+<meta property="og:site_name" content="{{ $meta_details['tournament_name'] }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:image" content="{{ $meta_details['open_graph_image'] }}" />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="635" />
-<meta name='twitter:card' content="summary">
-<meta name='twitter:url' content="{{ url()->current() }}">
-<meta name='twitter:title' content="{{ $meta_details['tournament_name'] }} - {{ $pageTitle }}">
-<meta name='twitter:description' content="{{ $meta_details['description'] }}">
-<meta name='twitter:image' content="{{ $meta_details['open_graph_image'] }}">
+<meta name='twitter:card' content="summary" />
+<meta name='twitter:url' content="{{ url()->current() }}" />
+<meta name='twitter:title' content="{{ $meta_details['tournament_name'] }} - {{ $pageTitle }}" />
+<meta name='twitter:description' content="{{ $meta_details['description'] }}" />
+<meta name='twitter:image' content="{{ $meta_details['open_graph_image'] }}" />
 <meta name='twitter:creator' content="">
 <!-- iOS Safari -->
 <meta name="apple-mobile-web-app-capable" content="yes">
