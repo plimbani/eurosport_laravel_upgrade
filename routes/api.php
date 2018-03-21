c7c528f53c1e9989cbb22ccb888da285f6951c96
Usama <upatel@aecordigital.com>
2018-03-21 11:05:28 +0530

    Extra route removed

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,6 @@ $api->version('v1',['middleware' => 'jwt.auth'], function ($api) {
     $api->post('match/saveResult', 'Laraspace\Api\Controllers\MatchController@saveResult');
     $api->post('match/saveAllResults', 'Laraspace\Api\Controllers\MatchController@saveAllResults');
     $api->post('match/getUnavailableBlock', 'Laraspace\Api\Controllers\MatchController@getUnavailableBlock');
-    $api->post('match/remove_block/{blockId}', 'Laraspace\Api\Controllers\MatchController@removeBlock');
     $api->post('match/updateScore', 'Laraspace\Api\Controllers\MatchController@updateScore');
 
     $api->post('match/checkTeamIntervalforMatches',
