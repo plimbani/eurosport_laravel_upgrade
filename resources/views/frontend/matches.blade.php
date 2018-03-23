f88e09fa5487194e6fe131b73144261c95d0273f
Shreya <sshah@aecordigital.com>
2018-03-23 16:18:07 +0530

    Match Table & Program Table

--- resources/views/frontend/matches.blade.php
+++ resources/views/frontend/matches.blade.php
@@ -10,17 +10,74 @@
 	<!-- Content wrapper -->
 	<div class="content__wrapper">
     <div class="container">
-      <div class="row my-5">
-        <div class="grid-22">
+        {{-- <div class="row my-5">
+            <div class="grid-22">
+            </div>
+            <div class="col-lg-8 club_content" id="matches_list">
+              @if($tournament)
+                  <match-listing></match-listing>
+              @else
+                  {!! __('messages.match_schedule_message') !!}
+              @endif
+            </div>
+        </div> --}}
+
+        <div class="row my-5">
+            <div class="col-lg-12 club_content macth_table" id="matches_list">
+                <h4>Table Title</h4>
+                <hr class="hr m-0">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Category</th>
+                                <th scope="col">Born on or after</th>
+                                <th scope="col">Number of teams</th>
+                                <th scope="col">Game length</th>
+                                <th scope="col">Players per team</th>
+                                <th scope="col">Number of matches</th>
+                                <th scope="col">Substitues allowed</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>U19</td>
+                                <td>01/01/1999</td>
+                                <td><a href="#">16</a></td>
+                                <td>2 * 15</td>
+                                <td>11</td>
+                                <td>5</td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>U19</td>
+                                <td>01/01/1999</td>
+                                <td>16</td>
+                                <td>2 * 15</td>
+                                <td>11</td>
+                                <td>5</td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>U19</td>
+                                <td>01/01/1999</td>
+                                <td>16</td>
+                                <td>2 * 15</td>
+                                <td>11</td>
+                                <td>5</td>
+                                <td>5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                @if($tournament)
+                    <match-listing></match-listing>
+                @else
+                    {!! __('messages.match_schedule_message') !!}
+                @endif
+            </div>
         </div>
-        <div class="col-lg-8 club_content" id="matches_list">
-          @if($tournament)
-              <match-listing></match-listing>
-          @else
-              {!! __('messages.match_schedule_message') !!}
-          @endif
-        </div>
-      </div>
     </div>
 	</div>
 	<!-- End of content wrapper -->
