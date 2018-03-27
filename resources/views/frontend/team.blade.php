8a7901f7689097d9a9dce5bd3bbc7e1d0a94c283
Shreya <sshah@aecordigital.com>
2018-03-27 17:19:02 +0530

    Tournament List View

--- resources/views/frontend/team.blade.php
+++ resources/views/frontend/team.blade.php
@@ -11,23 +11,24 @@
 	<div class="content__wrapper">
 	    <div class="container">
 	        <div class="row my-5">
-	            <div class="col-lg-3">
-	            </div>
-	            <div class="col-lg-8 club_content">
-									<div class="row">
-											@foreach($ageCategories as $category)
-													<div class="col-sm-3">
-															<h3>{{ $category->name }}</h3>
-															<div>
-																	<ul class="js-list">
-																			@foreach($category->teams as $team)
-																					<li class="team-item">{{ $team->name }} ({{ $team->country->country_code }}) <span class="flag-icon flag-icon-{{ $team->country->country_flag }}"></span></li>
-																			@endforeach
-																	</ul>
-															</div>
-													</div>
-											@endforeach
-									</div>
+	            {{-- <div class="col-lg-3">
+	            </div> --}}
+	            <div class="col-lg-12 club_content tournament-list">
+					<div class="row">
+						@foreach($ageCategories as $category)
+							<div class="col-sm-3">
+								<h3 class="mb-0">{{ $category->name }}</h3>
+								<hr class="hr mt-0 mb-3">
+								<div>
+									<ul class="js-list">
+										@foreach($category->teams as $team)
+											<li class="team-item"><span>{{ $team->name }} ({{ $team->country->country_code }})</span> <span class="flag-icon pull-right flag-icon-{{ $team->country->country_flag }}"></span></li>
+										@endforeach
+									</ul>
+								</div>
+							</div>
+						@endforeach
+					</div>
 	            </div>
 	        </div>
 	    </div>
