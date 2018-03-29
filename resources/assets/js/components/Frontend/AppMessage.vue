1eefbe062d73d4d9b9f74bac9656de6a2af874ce
Shreya <sshah@aecordigital.com>
2018-03-29 11:58:12 +0530

    Design Changes & Venue

--- resources/assets/js/components/Frontend/AppMessage.vue
+++ resources/assets/js/components/Frontend/AppMessage.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="position-absolute main-alert-wrapper custom-alert">
-        <div class="alert alert-dismissible alert-warning fade mb-2 show text-primary" role="alert" v-for="message in recent_messages" :key="message.id">
+        <div class="alert alert-dismissible alert-warning bg-shade-50 border-0 fade mb-2 show text-white" role="alert" v-for="message in recent_messages" :key="message.id">
           {{ message.content }}
-          <button type="button" class="close p-0" data-dismiss="alert" aria-label="Close" @click="dismissMessage(message.id)">
-            <span aria-hidden="true">&times;</span>
+          <button type="button" class="close p-0 h7 font-weight-normal" data-dismiss="alert" aria-label="Close" @click="dismissMessage(message.id)">
+            <span aria-hidden="true"><i class="fas fa-times font-weight-normal"></i></span>
           </button>
         </div>
     </div>
