bcf886437944be916193ac9afb027711d151247e
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-03-30 15:00:53 +0530

    frontend changes

--- resources/views/partials/frontend/tournament-messages.blade.php
+++ resources/views/partials/frontend/tournament-messages.blade.php
@@ -1,7 +1,3 @@
 <div id="app_messages">
 	<app-messages :recent_messages="recent_messages" @updaterecentmessages="updateRecentMessages"></app-messages>
 </div>
-
-<script type="text/javascript">
-	
-</script>
