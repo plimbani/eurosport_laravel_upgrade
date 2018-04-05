443fb5421b404388c4424acb87349fc1abfe5e2f
Usama <upatel@aecordigital.com>
2018-04-05 15:54:07 +0530

    Ticket #379 change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
         Commands\automaticallyPermissionRemoval::class,
         Commands\generatePositionsForExistingData::class,
         Commands\sendActivityNotification::class,
-        Commands\removeDanglingImages::class,
+        // Commands\removeDanglingImages::class,
     ];
 
     /**
