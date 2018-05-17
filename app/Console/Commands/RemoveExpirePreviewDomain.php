da55672b9d2869c40a57d997455fd8a20e55c467
rishabh <rishabh>
2018-05-17 16:10:46 +0530

    Ticket 450 changes

---
+++ app/Console/Commands/RemoveExpirePreviewDomain.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Laraspace\Console\Commands;
+
+use Illuminate\Console\Command;
+use Laraspace\Models\Website;
+use Carbon\Carbon;
+
+class RemoveExpirePreviewDomain extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'remove:previewUrl';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $websites = Website::whereNotNull('preview_domain')->get();
+       
+        foreach ($websites as $key => $website) {
+            $currentDate = Carbon::now();
+            $previewDomainGenerateDate = Carbon::parse($website->preview_domain_generated_at);
+            $diffInMinutes = $currentDate->diffInMinutes($previewDomainGenerateDate);
+
+            if($diffInMinutes >= config('config-variables.preview_url_expire_time')) {
+                $website->preview_domain = null;
+                $website->preview_domain_generated_at = null;
+                $website->save();
+            }
+            
+        }
+
+    }
+}
--- config/config-variables.php
+++ config/config-variables.php
@@ -5,5 +5,6 @@ return [
     "is_score_auto_update" => env('SCORE_AUTO_UPDATE'),
     "is_automate_match_schedule_enabled" => env('AUTOMATE_MATCH_SCHEDULING_ENABLED'),
     "reset_password_interval" => env('RESET_PASSWORD_INTERVAL'),
-    "signed_url_interval" => env('SIGNED_URL_INTERVAL')
+    "signed_url_interval" => env('SIGNED_URL_INTERVAL'),
+    "preview_url_expire_time" => env('PREVIEW_URL_EXPIRE_TIME')
 ];
