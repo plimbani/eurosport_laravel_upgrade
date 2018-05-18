72231ef2305c07328baefdff4552940383e9eaab
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-05-18 16:46:07 +0530

    ticket 450 changes

--- resources/views/partials/frontend/header-menu.blade.php
+++ resources/views/partials/frontend/header-menu.blade.php
@@ -1,4 +1,5 @@
 <!-- Navbar -->
+@php($websiteDomain = $isWebsiteInPreview ? $websiteDetail->preview_domain : $websiteDetail->domain_name)
 <nav class="navbar navbar-expand-lg navbar-primary js-header-menu-section">
     <div class="container">
         <div class="d-lg-none">
@@ -20,25 +21,25 @@
                         @php($childRoutes = array_key_exists($item['name'], config('wot.parents_child_routes')) ? config('wot.parents_child_routes.' . $item['name']) : [])
                         <li class="nav-item{{ (isset($item['children']) && count($item['children']) > 0) ? ' dropdown' : '' }}{{ (in_array(Route::currentRouteName(), $item['accessible_routes']) || in_array(Route::currentRouteName(), $childRoutes)) ? ' active' : '' }}">
                             @if(isset($item['children']) && count($item['children']) > 0)
-                                <a class="nav-link dropdown-toggle" href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDetail->domain_name]) }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{!! __('messages.' . $item['name']) !!}</a>
+                                <a class="nav-link dropdown-toggle" href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDomain]) }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{!! __('messages.' . $item['name']) !!}</a>
                                 <div class="dropdown-menu">
                                     <ul>
                                         <li class="nav-value d-none d-lg-block">
-                                            <a href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDetail->domain_name]) }}" class="current-tab">{!! __('messages.' . $item['name']) !!}</a>
+                                            <a href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDomain]) }}" class="current-tab">{!! __('messages.' . $item['name']) !!}</a>
                                         </li>
                                         @foreach($item['children'] as $childItem)
                                             <li>
                                                 @if($childItem['is_additional_page'] == 1)
                                                     <a href="{{ '/' . LaravelLocalization::getCurrentLocale() .$childItem['url'] }}" class="{{ app()->request->route('additionalPageName') == $childItem['page_name'] ? 'active' : '' }}">{{ $childItem['title'] }}</a>
                                                 @else
-                                                    <a href="{{ route(config('wot.page_routes')[$childItem['name']], ['domain' => $websiteDetail->domain_name]) }}" class="{{ in_array(Route::currentRouteName(), $childItem['accessible_routes']) ? 'active' : '' }}">{!! __('messages.' . $childItem['name']) !!}</a>
+                                                    <a href="{{ route(config('wot.page_routes')[$childItem['name']], ['domain' => $websiteDomain]) }}" class="{{ in_array(Route::currentRouteName(), $childItem['accessible_routes']) ? 'active' : '' }}">{!! __('messages.' . $childItem['name']) !!}</a>
                                                 @endif
                                             </li>
                                         @endforeach
                                     </ul>
                                 </div>
                             @else
-                                <a class="nav-link" href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDetail->domain_name]) }}">{!! __('messages.' . $item['name']) !!}</a>
+                                <a class="nav-link" href="{{ route(config('wot.page_routes')[$item['name']], ['domain' => $websiteDomain]) }}">{!! __('messages.' . $item['name']) !!}</a>
                             @endif
                         </li>
                     @endif
