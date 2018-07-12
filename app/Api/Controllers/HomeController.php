e02c66d4553bbb3f389784f006389dbefd29ddaa
Sunny Sheth <ssheth@aecordigital.com>
2018-07-12 18:19:37 +0530

    API changes

--- app/Api/Controllers/HomeController.php
+++ app/Api/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace Laraspace\Api\Controllers;
 
 use Illuminate\Http\Request;
 use Laraspace\Models\Website;
+use Laraspace\Models\NewPage;
 use Laraspace\Http\Requests\Homepage\GetStatisticsRequest;
 use Laraspace\Http\Requests\Homepage\GetOrganisersRequest;
 use Laraspace\Http\Requests\Homepage\StoreUpdateRequest;
@@ -92,12 +93,66 @@ class HomeController extends BaseController
     $websites = Website::with('pages')->get();
     $allDefaultPages = config('wot.website_default_pages');
 
-    foreach ($websites as $key => $website) {
-      $websitePages = $website->pages;
-      foreach ($allDefaultPages as $key => $page) {
+    $navigationMenuReference = [
+      'home' => 'home',
+      'matches' => 'matches',
+      'tournament' => 'tournament',
+      'age_categories' => 'tournament',
+      'teams' => 'teams',
+      'rules' => 'rules',
+      'history' => 'history',
+      'program' => 'program',
+      'program_overview' => 'program',
+      'stay' => 'stay',
+      'visitors' => 'visitors',
+      'public_transport' => 'public_transport',
+      'tourist_information' => 'tourist_information',
+      'tips' => 'tips',
+      'accommodation' => 'accommodation',
+      'meals' => 'meals',      
+      'venue' => 'venue',
+      'media' => 'media',
+      'contact' => 'contact'
+    ];
+
+    foreach ($websites as $allWebsitKey => $website) {
+      $websitePages = $website->pages->keyBy('name');
+      foreach ($allDefaultPages as $defaultPageKey => $page) {
+        $pageName = $navigationMenuReference[$page['name']];
+        $this->saveNewPageData($websitePages[$pageName], $pageName);
         
+        if(array_key_exists('children', $page)) {
+          foreach ($page['children'] as $childPageKey => $child) {
+            $childPageName = $navigationMenuReference[$child['name']];
+            $this->saveNewPageData($websitePages[$childPageName], $pageName);
+          }
+        }
+
       }
     }
+  }
+
+  public function saveNewPageData($pageData, $pageName)
+  {
+    // $newPage = new NewPage();
+    // $newPage->url = 
+    // $newPage->page_name = 
+    // $newPage->website_id = 
+    // $newPage->parent_id = 
+    // $newPage->name = 
+    // $newPage->accessible_routes = 
+    // $newPage->title = 
+    // $newPage->content = 
+    // $newPage->order = 
+    // $newPage->meta = 
+    // $newPage->is_additional_page = 
+    // $newPage->is_enabled = 
+    // $newPage->is_published = 
+    // $newPage->created_by = 
+    // $newPage->updated_by = 
+    // $newPage->save();
+
+    // return $newPage;
   }
   
 }
