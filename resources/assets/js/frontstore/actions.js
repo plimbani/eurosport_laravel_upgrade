c41d129cc0182dd15cf11622674f36782e9ce76a
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-07-23 18:08:06 +0530

    changes

--- resources/assets/js/frontstore/actions.js
+++ resources/assets/js/frontstore/actions.js
@@ -1,9 +1,1 @@
 import * as types from './mutation-types.js'
-
-export const setCurrentScheduleView = ({ commit }, currentScheduleView) => {
-	commit(types.SET_CURRENT_SCHEDULE_VIEW, currentScheduleView)
-}
-
-export const setCurrentView = ({ commit }, currentView) => {
-	commit(types.SET_CURRENT_VIEW, currentView);
-}
