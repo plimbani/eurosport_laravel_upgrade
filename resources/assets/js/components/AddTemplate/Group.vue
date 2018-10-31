a69b2464e6a2140fa8584409283504fb5f60eb5c
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-10-31 12:41:26 +0530

    add template changes

--- resources/assets/js/components/AddTemplate/Group.vue
+++ resources/assets/js/components/AddTemplate/Group.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 	    <div class="bordered-box">
-	        <h6 class="font-weight-bold">Group {{ groupIndex + 1}} <span class="pull-right"><a href="javascript:void(0)" @click="removeGroup()"><i class="fa fa-trash"></i></a></span></h6>
+	        <h6 class="font-weight-bold">Group {{ index + 1}} <span class="pull-right"><a href="javascript:void(0)" @click="removeGroup()"><i class="fa fa-trash"></i></a></span></h6>
 	        <div class="form-group">
 	            <div class="radio">
 	                <label><input type="radio" name="grouping_method" checked="checked" value="round_robin" v-model="data.type"> Round robin</label>
