ff129e267937aff74489ba92555b12163d20bb0f
nitin <nitin.smartech@gmail.com>
2018-12-25 23:35:44 +0530

    added add tournament date integration and API integration

--- resources/assets/js/views/buylicense/Buylicense.vue
+++ resources/assets/js/views/buylicense/Buylicense.vue
@@ -7,8 +7,9 @@
         <!-- <router-link :to="{ name: 'bar', params: { id: 123 }}">bar</router-link> -->
         <div>
             <h3>NUMBER OF TEAMS COMPETING</h3>
-            <vue-slider v-model="tournamentData.teams"></vue-slider>
-        </div>
+            <vue-slider v-model="tournamentData.tournament_max_teams"></vue-slider>
+            <!-- <vue-slider :min='2' :max='60' v-model="tournamentData.tournament_max_teams"></vue-slider> -->
+                </div>
         <div>
             <h3>Name of your Tournament</h3>
             <input type="text" class="form-control form-control-danger" placeholder="Tournament Name" id = "tournament_name" name="tournament_name" v-model="tournamentData.tournament_name" v-validate="{ rules: { required: true } }">
@@ -17,13 +18,13 @@
 
         <div>
             <h4>Start Date</h4>
-           <!--  <date-picker placeholder="Start Date" lang="en" v-model="tournamentData.start_date" :first-day-of-week="1"></date-picker> -->
-            <datepicker :value="tournamentData.start_date" :disabled-dates="startDisabledDates"></datepicker>
+           <!--  <date-picker placeholder="Start Date" lang="en" v-model="tournamentData.tournament_start_date" :first-day-of-week="1"></date-picker> -->
+            <datepicker  @selected="selectStartDate" id="startDate" :value="tournamentData.tournament_start_date" :disabled-dates="startDisabledDates" :format="customFormatter" v-validate="{ rules: { required: true } }"></datepicker>
         </div>
         <div>
             <h4>End Date</h4>
-            <!-- <date-picker placeholder="End Date" lang="en" v-model="tournamentData.end_date" :first-day-of-week="1"></date-picker> -->
-            <datepicker :value="tournamentData.end_date" :disabled-dates="endDisabledDates"></datepicker>
+            <!-- <date-picker placeholder="End Date" lang="en" v-model="tournamentData.tournament_end_date" :first-day-of-week="1"></date-picker> -->
+            <datepicker @selected="selectEndDate" id="endDate" :value="tournamentData.tournament_end_date" :disabled-dates="endDisabledDates" :format="customFormatter" v-validate="{ rules: { required: true } }"></datepicker>
         </div>
 
         <button class="btn btn-login">Buy Your Licence</button>
@@ -31,8 +32,8 @@
 </template>
 <script type="text/babel">
     import Auth from '../../services/auth'
+    import Constant from '../../services/constant';
     import vueSlider from 'vue-slider-component';
-    // import DatePicker from 'vue2-datepicker'
     import Datepicker from 'vuejs-datepicker';
     // console.log("register  page");
     export default {
@@ -43,10 +44,13 @@
         data() {
             return {
               tournamentData:{
-                teams: 0,  
+                tournament_max_teams: 2,  
                 tournament_name: "",
-                start_date:"",  
-                end_date:"", 
+                // tournament_start_date:"12/25/2018",  
+                // tournament_end_date:"12/25/2018", 
+                tournament_start_date:new Date(),  
+                tournament_end_date:new Date(), 
+                total_amount:300, 
               },
               startDisabledDates:{
                 to: new Date(Date.now() - 8640000),
@@ -54,27 +58,53 @@
               endDisabledDates:{
                  to: new Date(Date.now() - 8640000),
                 // from: new Date(Date.now() - 8640000),
-                // from: new Date(this.tournamentData.start_date + 8640000),
+                // from: new Date(this.tournamentData.tournament_start_date + 8640000),
               } 
             }
         },
         beforeRouteEnter(to, from, next) {
               if(Object.keys(to.query).length !== 0) { //if the url has query (?query)
                 next(vm => {
-                    // console.log("to.query.teams:",to.query.teams);
-                    vm.tournamentData.teams = to.query.teams
+                    vm.tournamentData.tournament_max_teams = to.query.teams;
                })
             }
             next()
         },
         methods: {
+            selectStartDate(date){
+                this.tournamentData.tournament_start_date = date;
+            },
+            selectEndDate(date){
+                this.tournamentData.tournament_end_date = date;
+            },
             buyALicence(e){
                 this.$validator.validateAll();
                 if (!this.errors.any()) {
-                    console.log("buyALicence::",this.tournamentData);
+                    this.tournamentData.tournament_start_date = moment(this.tournamentData.tournament_start_date).format('MM/DD/YYYY')
+                    this.tournamentData.tournament_end_date = moment(this.tournamentData.tournament_end_date).format('MM/DD/YYYY')
+                    axios.post(Constant.apiBaseUrl+'buy-license', this.tournamentData).then(response =>  {
+                         if (response.data.success) {
+                            this.$router.push({'name':'welcome'})
+                         }else{
+                             toastr['error'](response.data.message, 'Error');
+                         }
+                     }).catch(error => {
+                         console.log("error in register::",error);
+                     });
                     // this.$router.push({'name':'welcome'}) 
                 }
+            },
+            customFormatter(date) {
+              return moment(date).format('MM/DD/YYYY');
             } 
         },
+        mounted() {
+
+            // $('#startDate').on('change', () => { 
+            //     this.tournamentData.tournament_start_date = $('#startDate').val() 
+            //     console.log("ff::",this.tournamentData.tournament_start_date)
+            // }
+            // )
+        }
     }
 </script>
