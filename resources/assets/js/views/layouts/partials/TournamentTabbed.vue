cffb29721690574afdc9e55d8abde0bbe4f91d41
Shreya <sshah@aecordigital.com>
2019-01-09 12:40:23 +0530

    Tabbing UI Changes

--- resources/assets/js/views/layouts/partials/TournamentTabbed.vue
+++ resources/assets/js/views/layouts/partials/TournamentTabbed.vue
@@ -4,26 +4,78 @@
       <div class="row">
         <div class="col-lg-12">
           <div class="tabs tabs-primary">
-            <ul class="nav nav-tabs" role="tablist">
-              <li class="nav-item">
-                <a :class="[activePath == 'tournament_add' ? 'active' : '', 'nav-link']" data-toggle="tab"  href="#tournament_add" role="tab" @click="GetSelectComponent('tournament_add')">{{$lang.tournament_label}}</a>
-              </li>
-              <li class="nav-item">
-                <a :class="[activePath == 'competation_format' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#competation_format" role="tab" @click="GetSelectComponent('competation_format')">{{$lang.competation_label}}</a>
-              </li>
-              <li class="nav-item">
-                <a :class="[activePath == 'pitch_capacity' ? 'active' : '', 'nav-link']" data-toggle="tab"
-                href="#pitch_capacity" role="tab" @click="GetSelectComponent('pitch_capacity')">{{$lang.pitch_capacity_label}}</a>
-              </li>
-              <li class="nav-item">
-                <a :class="[activePath == 'teams_groups' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#teams_groups" role="tab"  @click="GetSelectComponent('teams_groups')">{{$lang.teams_groups_label}}</a>
-              </li>
-              <li class="nav-item">
-                <a :class="[activePath == 'pitch_planner' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#pitch_planner" role="tab" @click="GetSelectComponent('pitch_planner')">{{$lang.pitch_planner_label}}</a>
-              </li>
-              <li class="nav-item">
-                <a :class="[activePath == 'tournaments_summary_details' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#home3" role="tab" @click="GetSelectComponent('tournaments_summary_details')">{{$lang.summary_label}}</a>
-              </li>
+            <ul class="nav nav-tabs edit-tournament-tab" role="tablist">
+                <li class="nav-item">
+                    <a :class="[activePath == 'tournament_add' ? 'active' : '', 'nav-link']" data-toggle="tab"  href="#tournament_add" role="tab" @click="GetSelectComponent('tournament_add')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+
+                        {{$lang.tournament_label}}
+
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="top" data-content="Add and edit tournament dates and specify location and contact details">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a> 
+                </li>
+
+                <li class="nav-item">                    
+                    <a :class="[activePath == 'competation_format' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#competation_format" role="tab" @click="GetSelectComponent('competation_format')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+
+                        {{$lang.competation_label}}
+
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="top" data-content="Create and edit age categories, competition structures, game duration details and set the rules">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a>                    
+                </li>
+
+                <li class="nav-item">                    
+                    <a :class="[activePath == 'pitch_capacity' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#pitch_capacity" role="tab" @click="GetSelectComponent('pitch_capacity')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+
+                        {{$lang.pitch_capacity_label}}
+
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="top" data-content="Add pitches, specify pitch size, type and confirm pitch availability">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a>                    
+                </li>
+
+                <li class="nav-item">                    
+                    <a :class="[activePath == 'teams_groups' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#teams_groups" role="tab"  @click="GetSelectComponent('teams_groups')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+
+                        {{$lang.teams_groups_label}}
+
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="top" data-content="Upload your teams, allocate teams to age categories and edit team colours">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a>                    
+                </li>
+
+                <li class="nav-item">                    
+                    <a :class="[activePath == 'pitch_planner' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#pitch_planner" role="tab" @click="GetSelectComponent('pitch_planner')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+
+                        {{$lang.pitch_planner_label}}
+        
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="top" data-content="Manually or automatically schedule games and allocate referees">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a>                    
+                </li>
+
+                <li class="nav-item">
+                    <a :class="[activePath == 'tournaments_summary_details' ? 'active' : '', 'nav-link']" data-toggle="tab" href="#home3" role="tab" @click="GetSelectComponent('tournaments_summary_details')">
+                        <span class="icon-football-block"><i class="fas fa-futbol"></i></span>
+                    
+                        {{$lang.summary_label}}
+
+                        <span class="text-tooltip" data-toggle="popover" data-animation="false" data-placement="left" data-content="Edit and view results, access reports and send messages to your tournament participants">
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    </a>                    
+                </li>
             </ul>
           <router-view></router-view>
           </div>
@@ -52,6 +104,19 @@ export default {
       }
     // alert('hi')
     // here we call function which select the active class
+    // 
+    $("[data-toggle=popover]").popover({
+        html : false,
+        trigger: 'hover',
+        content: function() {
+            var content = $(this).attr("data-popover-content");
+            return $(content).children(".popover-body").html();
+        },
+        title: function() {
+            var title = $(this).attr("data-popover-content");
+            return $(title).children(".popover-heading").html();
+        }
+    });
   },
   methods: {
     GetSelectComponent(componentName) {
