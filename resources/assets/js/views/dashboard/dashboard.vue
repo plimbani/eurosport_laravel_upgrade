2cc44470d89c85746d892e94d97bb43f293b3356
nitin <nitin.smartech@gmail.com>
2019-01-24 21:48:09 +0530

    dashboard page design integration

---
+++ resources/assets/js/views/dashboard/dashboard.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="manage-tournament section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="text-uppercase font-weight-bold mb-4">Manage Tournaments</h3>
+
+                    <div class="row">
+                        <div class="col-md-6 col-lg-12 d-flex">
+                            <div class="card w-100">
+                                <div class="card-block">
+                                    <div class="row align-items-center">
+                                        <div class="col-xl-7">
+                                            <p class="h7 text-uppercase mb-0">License: #000987</p>
+                                            <div class="row align-items-center mb-2">
+                                                <div class="col-lg-7">
+                                                    <h3 class="font-weight-bold mb-0">Brighton Champions Cup</h3>
+                                                </div>
+                                                <div class="col-lg-5">
+                                                    <ul class="list-unstyled mb-0 tournament-information">
+                                                        <li class="d-inline h7 text-uppercase font-weight-bold pr-2"><span><i class="fa fa-globe"></i></span>&nbsp; <a href="#">View public website</a></li>
+                                                        <li class="d-inline h7 text-uppercase font-weight-bold"><span><i class="fa fa-share-alt"></i></span>&nbsp; <a href="#">Share</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <ul class="list-unstyled mb-0">
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-list-ul" aria-hidden="true"></i> &nbsp;32 Teams</li>
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-futbol-o" aria-hidden="true"></i> &nbsp;48 Matches</li>
+                                                <li class="d-block d-lg-inline"><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp;19th - 22nd October 2018</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-xl-5 mt-3 mt-lg-0 text-lg-right">
+                                            <div class="btn-group">
+                                                <button class="btn btn-outline"><span><i class="fa fa-pencil" aria-hidden="true"></i></span>&nbsp; Edit</button>
+                                                <button class="btn btn-outline ml-2">Manage License</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-12 d-flex">
+                            <div class="card w-100">
+                                <div class="card-block">
+                                    <div class="row align-items-center">
+                                        <div class="col-xl-7">
+                                            <p class="h7 text-uppercase mb-0">License: #000987</p>
+                                            <div class="row align-items-center mb-2">
+                                                <div class="col-lg-7">
+                                                    <h3 class="font-weight-bold mb-0">Brighton Champions Cup</h3>
+                                                </div>
+                                                <div class="col-lg-5">
+                                                    <ul class="list-unstyled mb-0 tournament-information">
+                                                        <li class="d-inline h7 text-uppercase font-weight-bold pr-2"><span><i class="fa fa-globe"></i></span>&nbsp; <a href="#">View public website</a></li>
+                                                        <li class="d-inline h7 text-uppercase font-weight-bold"><span><i class="fa fa-share-alt"></i></span>&nbsp; <a href="#">Share</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <ul class="list-unstyled mb-0">
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-list-ul" aria-hidden="true"></i> &nbsp;32 Teams</li>
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-futbol-o" aria-hidden="true"></i> &nbsp;48 Matches</li>
+                                                <li class="d-block d-lg-inline"><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp;19th - 22nd October 2018</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-xl-5 mt-3 mt-lg-0 text-lg-right">
+                                            <div class="btn-group">
+                                                <button class="btn btn-outline"><span><i class="fa fa-pencil" aria-hidden="true"></i></span>&nbsp; Edit</button>
+                                                <button class="btn btn-outline ml-2">Manage License</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-success">Add Tournament</button>
+
+                    <h3 class="text-uppercase font-weight-bold mb-4 mt-5">Manage Templates</h3>
+                    <div class="row">
+                        <div class="col-md-6 col-lg-12 d-flex">
+                            <div class="card w-100">
+                                <div class="card-block">
+                                    <div class="row align-items-center">
+                                        <div class="col-xl-7">
+                                            <p class="h7 mb-0">Version 1</p>
+                                            <h3 class="font-weight-bold mb-2">Ben’s 4x4 + Knockout</h3>
+                                            <ul class="list-unstyled mb-0">
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-list-ul" aria-hidden="true"></i> &nbsp;32 Teams</li>
+                                                <li class="d-block d-lg-inline pb-1 pb-sm-0 pr-sm-2"><i class="fa fa-futbol-o" aria-hidden="true"></i> &nbsp;48 Matches</li>
+                                                <li class="d-block d-lg-inline"><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp;19th - 22nd October 2018</li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-xl-5 mt-3 mt-lg-0 text-lg-right">
+                                            <div class="btn-group">
+                                                <button class="btn btn-link"><span><i class="fa fa-trash" aria-hidden="true"></i></span>&nbsp;Delete</button>
+                                                <button class="btn btn-outline ml-2"><span><i class="fa fa-pencil" aria-hidden="true"></i></span>&nbsp; Edit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-success">Add Template</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script type="text/babel">
+    import Auth from '../../services/auth'
+    import Ls from '../../services/ls'
+    import Constant from '../../services/constant'
+
+    // console.log("register  page");
+    export default {
+        data() {
+            return { 
+                tournaments:[]
+            }
+        },
+        methods: {
+             
+             
+
+            printReceipt(){
+                 
+                
+                // this.$nextTick(() => {
+                //     window.print();
+                // });
+            }
+             
+            
+        },
+        beforeMount(){  
+             
+        }
+    }
+</script>
