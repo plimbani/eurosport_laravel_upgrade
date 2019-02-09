2b392e04e603c565740a944a39e2e45c5f0b9f8a
nitin <nitin.smartech@gmail.com>
2019-02-09 11:47:39 +0530

    new pages created

---
+++ resources/assets/js/views/userstournament/Userstournament.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="confirmation-section section-padding">
+        <div class="tournament-section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8">
+                        <div class="row">
+                            <div class="col-sm-4 col-md-3">
+                                <img src='/images/dummy.png' class="img-fluid tournament-image">
+                            </div>
+                            <div class="col-sm-8 col-md-9">
+                                 Users Tournaments
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+              
+            </div>
+        </div>
+    </section>
+</template>
+<script type="text/babel">
+    // console.log("register  page");
+    import Constant from '../../services/constant'
+    export default {
+        data() {
+            return {
+                usersTourmanents:{},
+                code:""
+            }
+        },
+        beforeRouteEnter(to, from, next) { 
+              if(Object.keys(to.query).length !== 0) { //if the url has query (?query)
+                next(vm => {    
+                    setTimeout(function(){   
+                        if(typeof to.query.code != "undefined"){
+                            vm.code = to.query.code;
+                            // console
+                            vm.getTournamentListOfUser();
+                        }
+                    }, 100); 
+               })
+            }
+            next()
+        },
+        methods: {
+            getTournamentListOfUser(){
+               
+            } 
+        },
+        beforeMount(){  
+            // this.getTournamentListOfUser();
+        }
+    }
+</script>
