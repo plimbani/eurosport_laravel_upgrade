e462ddcf0728e42e337e1a0642fb6c58dae16cf3
rishabh <rishabh>
2019-02-18 13:52:55 +0530

    Ticket 657 changes

--- resources/assets/js/components/BulkUnscheduledfixtureModal.vue
+++ resources/assets/js/components/BulkUnscheduledfixtureModal.vue
@@ -29,11 +29,6 @@
             },
             hideModal() {
                 $('#bulk_unscheduled_fixtures').modal('hide')
-                $("#unschedule_fixtures").html('Unschedule fixture').removeClass('btn btn-success');
-                $("#unschedule_fixtures").addClass('btn btn-primary btn-md btn-secondary');
-                $(".checkbox").addClass('d-none');
-                $("#cancle_unscheduling_fixtures").hide();
-                $(".match-unschedule-checkbox" ).prop( "checked", false);
                 return false
             }
         }
