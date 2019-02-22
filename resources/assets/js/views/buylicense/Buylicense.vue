ab74002f55f8d3c9b6ca1a2183352ba011260485
nitin <nitin.smartech@gmail.com>
2019-02-22 22:19:02 +0530

    added changes for the update tournamnt

--- resources/assets/js/views/buylicense/Buylicense.vue
+++ resources/assets/js/views/buylicense/Buylicense.vue
@@ -190,22 +190,18 @@
 
             getTournamentDetail(){ 
                 axios.get(Constant.apiBaseUrl+'get-tournament?tournamentId='+this.id, {}).then(response =>  {  
-                        if (response.data.success) { 
-                             // this.tournaments = response.data.data;
-                             // console.log("tournament details::",response.data.data)
-                             // for(let eachKey in response.data.data){
-                             //    this.tournamentData[eachKey] = response.data.data[eachKey]
-                             // } 
-                             var start_date = new Date(moment(response.data.data.start_date, 'DD/MM/YYYY').format('MM/DD/YYYY'));
-                            var end_date = new Date(moment(response.data.data.end_date, 'DD/MM/YYYY').format('MM/DD/YYYY'));
+                        if (response.data.success) {  
+                            var start_date = new Date(moment(response.data.data.start_date, 'DD/MM/YYYY').format('MM/DD/YYYY'));
+                            var end_date = new Date(moment(response.data.data.end_date, 'DD/MM/YYYY').format('MM/DD/YYYY')); 
                             this.tournamentData['id'] = this.id;
                             this.tournamentData['tournament_name'] = response.data.data.name;
-                            this.tournamentData['tournament_max_teams'] = response.data.data.maximum_teams;                            
-                            this.tournamentData['tournament_start_date'] = start_date.getMonth()+ 1 + '/'+start_date.getDate()+'/'+start_date.getFullYear();
-                            this.tournamentData['tournament_end_date'] = end_date.getMonth()+ 1 + '/'+end_date.getDate()+'/'+end_date.getFullYear();;
-                            // console.log("this.tournamentData:",this.tournamentData);
-                    //         tournament_start_date:new Date(),  
-                    // tournament_end_date:new Date(),
+                            this.tournamentData['tournament_max_teams'] = response.data.data.maximum_teams;   
+                            let startMonth = start_date.getMonth()+1;                         
+                            let endMonth = end_date.getMonth()+1;                         
+                            this.tournamentData['tournament_start_date'] = start_date.getDate()+'/'+startMonth + '/'+start_date.getFullYear();
+                            this.tournamentData['tournament_end_date'] = end_date.getDate()+'/'+endMonth + '/'+end_date.getFullYear(); 
+                            $('#tournament_start_date').datepicker('setDate', this.tournamentData['tournament_start_date'])
+                             $('#tournament_end_date').datepicker('setDate', this.tournamentData['tournament_end_date'])  
                          }else{ 
                             toastr['error'](response.data.message, 'Error');
                          }
