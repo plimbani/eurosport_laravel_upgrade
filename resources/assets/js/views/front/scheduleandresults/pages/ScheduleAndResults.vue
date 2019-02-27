d5513e3a93b6971c57fdd1c39eda101750e1e84f
rishabh <rishabh>
2019-02-27 15:20:57 +0530

    Ticket 600 text changes

--- resources/assets/js/views/front/scheduleandresults/pages/ScheduleAndResults.vue
+++ resources/assets/js/views/front/scheduleandresults/pages/ScheduleAndResults.vue
@@ -9,16 +9,16 @@
                 <div class="tabs tabs-primary">
 					<ul class="nav nav-tabs mb-4">
 						<li @click="setCurrentView('drawsListing')" class="nav-item">
-							<a :class="[currentView == 'drawsListing' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Catrgory</a>
+							<a :class="[currentView == 'drawsListing' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Categories</a>
 						</li>
 						<li @click="setCurrentView('matchListing')" class="nav-item">
-							<a :class="[currentView == 'matchListing' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Match</a>
+							<a :class="[currentView == 'matchListing' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Matches</a>
 						</li>
 						<li @click="setCurrentView('teamListing')" class="nav-item">
 							<a :class="[currentView == 'teamListing' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Teams</a>
 						</li>
 						<li @click="setCurrentView('finalPlacings')" class="nav-item">
-							<a :class="[currentView == 'finalPlacings' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Final placing</a>
+							<a :class="[currentView == 'finalPlacings' ? 'active' : '']" href="javascript:void(0)" class="nav-link">Final placings</a>
 						</li>
 					</ul>
 					<div class="tab-content">
