540cf83ad37d55f52542a9eb0a548f617ee980b1
miteshaecor <Mshah@aecordigital.com>
2019-02-28 21:38:39 +0530

    Move settings in new files

--- routes/commercialisation.php
+++ routes/commercialisation.php
@@ -26,8 +26,8 @@ Route::group(['middleware' => 'jwt.auth'], function() {
     Route::post('v1/manage-tournament', '\Laraspace\Api\Controllers\Commercialisation\TournamentController@manageTournament');
     Route::post('v1/customer-tournament', '\Laraspace\Api\Controllers\Commercialisation\TournamentController@getTournamentByCustomer');
     Route::post('v1/customer-transactions', '\Laraspace\Api\Controllers\Commercialisation\BuyLicenseController@getCustomerTransactions');
-    Route::post('v1/website-settings/save', '\Laraspace\Api\Controllers\WebsiteController@saveSettings');
-    Route::get('v1/get-website-settings', '\Laraspace\Api\Controllers\WebsiteController@getSettings');
+    Route::post('v1/website-settings/save', '\Laraspace\Api\Controllers\Commercialisation\WebsiteSettingsController@saveSettings');
+    Route::get('v1/get-website-settings', '\Laraspace\Api\Controllers\Commercialisation\WebsiteSettingsController@getSettings');
 });
 
 Route::post('v1/generateHashKey', '\Laraspace\Api\Controllers\Commercialisation\BuyLicenseController@generateHashKey');
