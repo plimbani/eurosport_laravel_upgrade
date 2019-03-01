72936b60a4341fbde09b4b16e7e0a78e0ed55f7f
rishabh <rishabh>
2019-03-01 13:11:10 +0530

    Ticket 600 changes

--- resources/assets/js/views/front/scheduleandresults/pages/list/components/Matches.vue
+++ resources/assets/js/views/front/scheduleandresults/pages/list/components/Matches.vue
@@ -8,8 +8,8 @@
               <th scope="col">Date and time</th>
               <th scope="col">Categories</th>
               <th scope="col">Match codes</th>
-              <th scope="col">Teams</th>
-              <th scope="col">Teams</th>
+              <th scope="col">Team</th>
+              <th scope="col">Team</th>
               <th scope="col">Score</th>
               <th scope="col" v-if="showPlacingForMatch()">Placing</th>
               <th scope="col">Location</th>
