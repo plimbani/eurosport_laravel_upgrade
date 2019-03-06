6ec3514619b3140c2798b57f301f3e8d4b97daa7
Mukesh Tilokani <mtilokani@aecordigital.com>
2019-03-06 10:54:29 +0530

    changes

---
+++ resources/assets/js/views/layouts/partials/Commercialisation/Backend/SiteFooter.vue
@@ -0,0 +1,41 @@
+<template>    
+    <footer class="site-footer d-flex justify-content-center">
+        <div class="page-copyright align-self-center">
+            <small>Copyright {{currentYear}} TMP Applications BV. Developed  by <a href="http://aecordigital.com" target="_blank"><font color="#fff" style="text-decoration: underline">aecor</font></a>.</small>
+        </div>
+    </footer>
+</template>
+
+<script type="text/babel">
+    export default {
+        data() {
+            return {
+                'footer' : 'footer',
+                'currentYear': new Date().getFullYear()
+            }
+        },
+        methods: {
+            // changeFooterStyle() {
+            //     alert('h')
+                
+            // }
+        },
+        mounted() {
+            // this.changeFooterStyle();
+
+            $(window).resize(function() {
+                if ($(document).height() > $(window).height()) {
+                    $('.site-footer').removeClass('sticky');    
+                } else {
+                   $('.site-footer').addClass('sticky');
+                }
+            });
+            if ($(document).height() > $(window).height()) {
+                    $('.site-footer').removeClass('sticky');    
+                } else {
+                   $('.site-footer').addClass('sticky');
+                }
+                        
+        }
+    }
+</script>
---
+++ resources/assets/js/views/layouts/partials/Commercialisation/Frontend/SiteHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="w-100 header">
+    	<div class="container-fluid">
+    		<div class="row align-items-center">
+    			<div class="col-5 col-md-4 pl-0">
+    				<a href="#"><img src="/images/logo-emm.png" class="" alt="Easy Match Manager"></a>
+    			</div>
+    			<div class="col-7 col-md-8 text-right">
+    				<p class="text-uppercase mb-0">For help call <a href="tel:+44(0)1234 567 890" class="font-weight-bold ml-3">+44(0)1234 567 890</a></p>
+    			</div>
+    		</div>
+    	</div>
+    </header>
+</template>
+
+
+<script type="text/babel">
+
+</script>
