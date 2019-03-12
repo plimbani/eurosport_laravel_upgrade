0f0ca786c730a8b91726375b644179e423aeebd1
miteshaecor <Mshah@aecordigital.com>
2019-03-12 22:05:48 +0530

    Role added in register response

--- app/Api/Repositories/Commercialisation/RegisterRepository.php
+++ app/Api/Repositories/Commercialisation/RegisterRepository.php
@@ -65,7 +65,8 @@ class RegisterRepository
             
             return [
                 'user' => $user,
-                'token' => $token = JWTAuth::attempt(['email' => $newUser['email'], 'password' => $data['password']])
+                'role' => $user->roles()->first(),
+                'token' => JWTAuth::attempt(['email' => $newUser['email'], 'password' => $data['password']])
             ];
         } else {
             return false;
