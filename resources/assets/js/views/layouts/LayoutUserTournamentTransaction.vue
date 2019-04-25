f950bc7246fa72ee420223a8a0b020503da6a610
rishabh <rishabh>
2019-04-25 09:08:39 +0000

    layout changes

---
+++ resources/assets/js/views/layouts/LayoutUserTournamentTransaction.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<site-commercialisation-header></site-commercialisation-header>
+        <div class="main-content container-fluid" id="dashboardPage">
+            <breadCrum></breadCrum>
+            <TournamentTabbed></TournamentTabbed>
+        </div> 
+		<site-commercialisation-footer></site-commercialisation-footer>
+	</div>
+</template>
+
+<script type="text/babel">
+	import SiteCommercialisationHeader from './partials/Commercialisation/Frontend/SiteHeader.vue'
+    import SiteCommercialisationFooter from './partials/Commercialisation/Frontend/SiteFooter.vue'
+    import TournamentTabbed from './partials/TournamentTabbed.vue'
+    import BreadCrum from '../../components/UserTournamentTransactionBreadCrum.vue'
+    export default {
+    	data() {
+            return {
+                'header' : 'header'
+            }
+        },
+        components : {
+            SiteCommercialisationHeader, SiteCommercialisationFooter, BreadCrum, TournamentTabbed
+        }    
+    }
+</script>
