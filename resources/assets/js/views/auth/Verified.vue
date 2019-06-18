84a772da5bb3f9c05bc188f5fb3a046716820902
Usama <upatel@aecordigital.com>
2019-06-18 11:44:39 +0000

    layout change in verified blade

--- resources/assets/js/views/auth/Verified.vue
+++ resources/assets/js/views/auth/Verified.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <div class="alert alert-success margin-top-15 my-3" v-if="queryParams == 'reset password'">
-            <i class="fas fa-check text-success mr-2"></i>Your password has been updated.
-        </div>
-        <div class="alert alert-success margin-top-15 my-3 d-inline-flex align-items-start" v-else>
-            <i class="fas fa-check text-success mr-2"></i>
-            <p class="mb-1">Your account registration is complete. You can now login to the mobile app.</p>
-        </div>
+    <div class="main-section">
+        <section class="login-section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="alert alert-success margin-top-15 my-3" v-if="queryParams == 'reset password'">
+                        <i class="fas fa-check text-success mr-2"></i>Your password has been updated.
+                    </div>
+                    <div class="alert alert-success margin-top-15 my-3 d-inline-flex align-items-start" v-else>
+                        <i class="fas fa-check text-success mr-2"></i>
+                        <p class="mb-1">Your account registration is complete. You can now login to the mobile app.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
