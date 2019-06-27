d1d4c515918472a306991484ee6c1634c63e48ac
rishabh <rishabh>
2019-06-27 11:33:08 +0000

    Ticket 800 changes

--- resources/assets/js/views/front/scheduleandresults/pages/finalplacings/FinalPlacings.vue
+++ resources/assets/js/views/front/scheduleandresults/pages/finalplacings/FinalPlacings.vue
@@ -28,7 +28,7 @@
       <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
           <div class="modal-header">
-             <h5 class="modal-title font-weight-bold">Final placings</h5>
+             <h5 class="modal-title font-weight-bold">Final Placings</h5>
              <button type="button" class="close" data-dismiss="modal">
              <span aria-hidden="true">×</span>
              </button>
