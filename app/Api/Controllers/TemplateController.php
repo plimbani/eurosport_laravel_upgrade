a87c4e69c5d643c8e6e52b059fc30e3a26021846
Sunny Sheth <ssheth@aecordigital.com>
2019-07-24 11:16:13 +0000

    request added for template feature

--- app/Api/Controllers/TemplateController.php
+++ app/Api/Controllers/TemplateController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Laraspace\Models\TournamentTemplates;
 use Laraspace\Api\Contracts\TemplateContract;
+use Laraspace\Http\Requests\Template\EditRequest;
+use Laraspace\Http\Requests\Template\StoreRequest;
+use Laraspace\Http\Requests\Template\UpdateRequest;
+use Laraspace\Http\Requests\Template\DeleteRequest;
+use Laraspace\Http\Requests\Template\TemplateDetailRequest;
 
 class TemplateController extends BaseController
 {
@@ -27,7 +32,7 @@ class TemplateController extends BaseController
     /**
      * Get template detail
      */
-    public function getTemplateDetail(Request $request)
+    public function getTemplateDetail(TemplateDetailRequest $request)
     {
         return $this->templateObj->getTemplateDetail($request->all());
     }
@@ -43,7 +48,7 @@ class TemplateController extends BaseController
     /**
      * Save template data
      */
-    public function saveTemplateDetail(Request $request)
+    public function saveTemplateDetail(StoreRequest $request)
     {
         return $this->templateObj->saveTemplateDetail($request->all());
     }
@@ -51,7 +56,7 @@ class TemplateController extends BaseController
     /**
      * Delete template
      */
-    public function deleteTemplate(Request $request, $id)
+    public function deleteTemplate(DeleteRequest $request, $id)
     {
         return $this->templateObj->deleteTemplate($id);
     }
@@ -59,7 +64,7 @@ class TemplateController extends BaseController
     /**
      * Edit template
      */
-    public function editTemplate(Request $request, $id)
+    public function editTemplate(EditRequest $request, $id)
     {
         return $this->templateObj->editTemplate($id);
     }
@@ -67,7 +72,7 @@ class TemplateController extends BaseController
     /**
      * Update template
      */
-    public function updateTemplateDetail(Request $request)
+    public function updateTemplateDetail(UpdateRequest $request)
     {
         return $this->templateObj->updateTemplateDetail($request->all());
     }
