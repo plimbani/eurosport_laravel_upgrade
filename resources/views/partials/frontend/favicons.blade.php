7917db4f2eba6e438a292e06240cbf91cc25248c
Mukesh Tilokani <mtilokani@aecordigital.com>
2020-01-23 22:36:29 +0530

    changing logo

--- resources/views/partials/frontend/favicons.blade.php
+++ resources/views/partials/frontend/favicons.blade.php
@@ -33,21 +33,21 @@
 	<meta name="msapplication-TileImage" content="{{ config('filesystems.disks.s3.url') . config('wot.imagePath.favicon') . $websiteDetail->id . '/' . 'mstile-144x144.png' . '?' . date('U') }}">
 	<meta name="theme-color" content="#FFFFFF">
 @else
-	<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-57x57.png') }}">
-	<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-60x60.png') }}">
-	<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-72x72.png') }}">
-	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-76x76.png') }}">
-	<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-114x114.png') }}">
-	<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-120x120.png') }}">
-	<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-144x144.png') }}">
-	<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-152x152.png') }}">
-	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/apple-icon-180x180.png') }}">
-	<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/android-icon-192x192.png') }}">
-	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/favicon-32x32.png') }}">
-	<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/favicon-96x96.png') }}">
-	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/favicon-16x16.png') }}">
-	<link rel="manifest" href="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/manifest.json') }}">
-	<meta name='msapplication-TileImage' content="{{ asset('assets/img/favicons/'.config('config-variables.current_layout').'/ms-icon-144x144.png') }}">
+	<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/favicons/wot/apple-icon-57x57.png') }}">
+	<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/img/favicons/wot/apple-icon-60x60.png') }}">
+	<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/favicons/wot/apple-icon-72x72.png') }}">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/favicons/wot/apple-icon-76x76.png') }}">
+	<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/favicons/wot/apple-icon-114x114.png') }}">
+	<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/img/favicons/wot/apple-icon-120x120.png') }}">
+	<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/img/favicons/wot/apple-icon-144x144.png') }}">
+	<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/img/favicons/wot/apple-icon-152x152.png') }}">
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/wot/apple-icon-180x180.png') }}">
+	<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('assets/img/favicons/wot/android-icon-192x192.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicons/wot/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/favicons/wot/favicon-96x96.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicons/wot/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('assets/img/favicons/wot/manifest.json') }}">
+	<meta name='msapplication-TileImage' content="{{ asset('assets/img/favicons/wot/ms-icon-144x144.png') }}">
 	<meta name='msapplication-TileColor' content="#667eea">
 	<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
 @endif
