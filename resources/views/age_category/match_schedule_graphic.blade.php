e853333bad4f30ed53b2ca68625cedd403319efa
Mukesh Tilokani <mtilokani@aecordigital.com>
2020-02-27 22:57:13 +0530

    issue solving

--- resources/views/age_category/match_schedule_graphic.blade.php
+++ resources/views/age_category/match_schedule_graphic.blade.php
@@ -2,8 +2,8 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <!-- <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet"> -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> -->
+    {{-- <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> --}}
     <title>Match Schedule – Template {{ $templateData['tournament_name'] }}</title>
     <style type="text/css">
       html {
