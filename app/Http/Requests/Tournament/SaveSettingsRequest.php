fc41ed42245054fbd76b91bc2ba47ca49470d03f
Usama Patel <upatel@aecordigital.com>
2020-04-23 12:41:39 +0530

    Ticket 894

---
+++ app/Http/Requests/Tournament/SaveSettingsRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laraspace\Http\Requests\Tournament;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaveSettingsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $data = $this->all();
+        if (isset($data['tournamentData']['screenRotateTime']) && $data['tournamentData']['tournamentId'] != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
