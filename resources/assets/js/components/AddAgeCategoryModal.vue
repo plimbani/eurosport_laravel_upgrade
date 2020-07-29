f544c7d8cb8cad3c60e7b03244adadefcf04f466
Usama Patel <upatel@aecordigital.com>
2020-07-29 17:53:28 +0530

    Bug fixes

--- resources/assets/js/components/AddAgeCategoryModal.vue
+++ resources/assets/js/components/AddAgeCategoryModal.vue
@@ -426,7 +426,7 @@
                 </div>
               </div>
               <div class="form-group row align-items-center">
-                <label class="col-sm-4 form-control-label">Minimum team match interval</label>
+                <label class="col-sm-4 form-control-label">Minimum team match interval*</label>
                 <div class="col-sm-8">
                   <div class="row align-items-center">
                     <div class="col-sm-4">
@@ -441,7 +441,7 @@
                 </div>
               </div>
               <div class="form-group row align-items-center">
-                <label class="col-sm-4 form-control-label">Maximum team match interval</label>
+                <label class="col-sm-4 form-control-label">Maximum team match interval*</label>
                 <div class="col-sm-8">
                   <div class="row align-items-center">
                     <div class="col-sm-4">
