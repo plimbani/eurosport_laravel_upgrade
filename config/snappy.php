a875cb27494a4ac2b3213027bc28c98cffbb39de
Mukesh Tilokani <mtilokani@aecordigital.com>
2020-12-06 16:48:38 +0530

    issue solving

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf"',
+        'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
