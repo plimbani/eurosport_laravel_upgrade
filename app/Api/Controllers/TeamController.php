6196c6f72985bce74cd348eb67b7e4d4b54b8f05
Mukesh Tilokani <mtilokani@aecordigital.com>
2020-12-17 01:18:38 +0530

    issue solving

--- app/Api/Controllers/TeamController.php
+++ app/Api/Controllers/TeamController.php
@@ -323,7 +323,7 @@ class TeamController extends BaseController
         ksort($reportData);
         $reportData  = http_build_query($reportData);
 
-        $signedUrl = UrlSigner::sign(url('api/teams/getTeamsFairPlayData/report/reportExport?' . $reportData), Carbon::now()->addMinutes(config('config-variables.signed_url_interval')));
+        $signedUrl = UrlSigner::sign(secure_url('api/teams/getTeamsFairPlayData/report/reportExport?' . $reportData), Carbon::now()->addMinutes(config('config-variables.signed_url_interval')));
 
         return $signedUrl;
     }
