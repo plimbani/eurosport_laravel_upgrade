a3df35860caed0324ce4fea48e3fd56a5838073d
vparmar-aecor <vparmar@aecordigital.com>
2022-11-21 16:39:36 +0530

    TICKET ES-1412 FIXES

---
+++ app/Http/Requests/User/SetFavouriteTeamRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laraspace\Http\Requests\User;
+
+//use Illuminate\Foundation\Http\FormRequest;
+use Dingo\Api\Http\FormRequest;
+
+
+class SetFavouriteTeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (app('request')->header('ismobileuser')) {
+            $isMobileUser = app('request')->header('ismobileuser');
+            if ($isMobileUser == "true") {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'tournament_id' => 'required|exists:tournaments,id',
+            'team_id' => 'required|exists:teams,id',
+        ];
+    }
+
+}
