ba01593ec6945072d75af75ef9c01953e473761f
vparmar-aecor <vparmar@aecordigital.com>
2023-04-08 15:02:39 +0530

    Revert log error fixes

--- app/Http/Requests/Match/GetDrawsRequest.php
+++ app/Http/Requests/Match/GetDrawsRequest.php
@@ -23,11 +23,18 @@ class GetDrawsRequest extends FormRequest
             
             if(isset($this->all()['tournamentId'])) {
                 $tournament_id = $this->all()['tournamentId'];
-            } else if (isset($this->all()['tournament_id'])) {
-                $tournament_id = $this->all()['tournament_id'];
             } else {
-                return false;
+                $tournament_id = $this->all()['tournament_id'];
             }
+            
+            
+            // if(isset($this->all()['tournamentId'])) {
+            //     $tournament_id = $this->all()['tournamentId'];
+            // } else if (isset($this->all()['tournament_id'])) {
+            //     $tournament_id = $this->all()['tournament_id'];
+            // } else {
+            //     return false;
+            // }
 
             $tournament = Tournament::where('id',$tournament_id)->first();
             $isTournamentPublished = $this->isTournamentPublished($tournament);
