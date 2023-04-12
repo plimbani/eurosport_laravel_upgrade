cd19986b3a735b3a91079ad910d75afb7742525b
vparmar-aecor <vparmar@aecordigital.com>
2023-04-12 18:28:16 +0530

    TICKET ES-1436

--- resources/assets/js/views/layouts/partials/UserManagementabbed.vue
+++ resources/assets/js/views/layouts/partials/UserManagementabbed.vue
@@ -7,12 +7,16 @@
 						<div class="col-lg-12">
 							<div class="tabs tabs-primary user_tabs">
 								<ul class="nav nav-tabs" role="tablist">
-									<li class="nav-item">
-										<a class="nav-link active" data-toggle="tab"
-										href="javascript:void(0)" role="tab"><div class="wrapper-tab">{{$lang.user_management_user}}</div></a>
+									<li @click="setCurrentView('adminUsers')" class="nav-item">
+										<a :class="[currentView == 'adminUsers' ? 'active' : '']" class="nav-link" data-toggle="tab"
+										href="javascript:void(0)" role="tab"><div class="wrapper-tab">{{$lang.user_management_admin_user}}</div></a>
+									</li>
+									<li @click="setCurrentView('mobileUsers')" class="nav-item">
+										<a :class="[currentView == 'mobileUsers' ? 'active' : '']" class="nav-link" data-toggle="tab"
+										href="javascript:void(0)" role="tab"><div class="wrapper-tab">{{$lang.user_management_mobileuser}}</div></a>
 									</li>
 								</ul>
-								<UserList :userList="userList" :isListGettingUpdate="isListGettingUpdate"></UserList>
+								<UserList :currentView="currentView" :userList="userList" :isListGettingUpdate="isListGettingUpdate"></UserList>
 							</div>
 						</div>
 					</div>
@@ -39,10 +43,16 @@ export default {
 				'userCount': 0,
       		},
       		isListGettingUpdate: false,
+			currentView: 'adminUsers',
 		}
 	},
 	components: {
 		UserList
+	},
+	watch: {
+		currentView(newVal, oldVal) {
+			this.getSelectComponent();
+		}
 	},
 	created() {
 		this.getSelectComponent();
@@ -51,76 +61,83 @@ export default {
 	},
 
 	methods: {
-	  clearSearch() {
-	    this.getSelectComponent()
-	},		
-	getSelectComponent(userSearh='', userType='', currentPage=1, noOfRecords=20) {
-		if (cancel !== undefined) {
-	        cancel();
-	    }
-	    this.isListGettingUpdate = true;
-	    $("body .js-loader").removeClass('d-none');
+	  	clearSearch() {
+	    	this.getSelectComponent()
+		},		
+		getSelectComponent(userSearh='', userType='', currentPage=1, noOfRecords=20) {
+			if (cancel !== undefined) {
+				cancel();
+			}
+			this.isListGettingUpdate = true;
+			$("body .js-loader").removeClass('d-none');
 
-		let userData = {}
+			let userData = {}
 
-		if(userSearh != '') {
-			  userData.userData = userSearh;
-		}
+			if(userSearh != '') {
+				userData.userData = userSearh;
+			}
 
-		if(userType != '') {
-			  userData.userType = userType;
-		}
+			if(userType != '') {
+				userData.userType = userType;
+			}
 
-		userData.currentPage = currentPage;
+			if (this.currentView == 'mobileUsers') {
+				userData.userType = 'mobile.user';
+			}
 
-		userData.noOfRecords = noOfRecords;
+			userData.currentPage = currentPage;
 
-	  axios.post('api/users/getUsersByRegisterType', userData, {cancelToken: new CancelToken(function executor(c) 
-	        {
-	            cancel = c;
-	        })
-	    }).then((response) => {
-	        if('data' in response.data) {
-	        	this.userList = response.data;
-	            this.userList.userCount = response.data.data.length;
-	          } else {
-	          	this.userList = [];
-	          	this.userList.userCount = 0;
-	          }
-	          this.isListGettingUpdate = false;
-	          $("body .js-loader").addClass('d-none');
-		    }).catch((error) => {
-		        if (axios.isCancel(error)) {
-		        }
-		        this.isListGettingUpdate = false;
-		        $("body .js-loader").addClass('d-none');
-		    });
+			userData.noOfRecords = noOfRecords;
 
-      // User.getUsersByRegisterType(userData).then(
-      //   (response)=> {
-
-          
-      //   },
-      //   (error)=> {
-
-      //   }
-      // )
-
-      // this.$router.push({name: 'users_list', params: { registerType: registerType }})
-			/*axios.get("/api/getUsersByRegisterType/"+registerType).then((response) => {
-
-				if('users' in response.data) {
-
-					this.userList.userData = response.data.users;
-                	this.userList.userCount = response.data.users.length;
+			axios.post('api/users/getUsersByRegisterType', userData, {cancelToken: new CancelToken(function executor(c) 
+				{
+					cancel = c;
+				})
+			}).then((response) => {
+				if('data' in response.data) {
+					this.userList = response.data;
+					this.userList.userCount = response.data.data.length;
 				} else {
-					this.userList.userData = [];
-                	this.userList.userCount = 0;
+					this.userList = [];
+					this.userList.userCount = 0;
 				}
-            });
-			this.$router.push({name: 'users_list', params: { registerType: registerType }})
-      */
-		}
+				this.isListGettingUpdate = false;
+				$("body .js-loader").addClass('d-none');
+			}).catch((error) => {
+				if (axios.isCancel(error)) {
+				}
+				this.isListGettingUpdate = false;
+				$("body .js-loader").addClass('d-none');
+			});
+
+				// User.getUsersByRegisterType(userData).then(
+				//   (response)=> {
+
+					
+				//   },
+				//   (error)=> {
+
+				//   }
+				// )
+
+				// this.$router.push({name: 'users_list', params: { registerType: registerType }})
+					/*axios.get("/api/getUsersByRegisterType/"+registerType).then((response) => {
+
+						if('users' in response.data) {
+
+							this.userList.userData = response.data.users;
+							this.userList.userCount = response.data.users.length;
+						} else {
+							this.userList.userData = [];
+							this.userList.userCount = 0;
+						}
+					});
+					this.$router.push({name: 'users_list', params: { registerType: registerType }})
+				*/
+		},
+		setCurrentView(currentView) {
+			this.currentView = currentView;
+		},
 	}
 }
 </script>
