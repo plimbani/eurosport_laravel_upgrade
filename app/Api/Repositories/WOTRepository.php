7a9be6d556abfb094a5bc15e0b8b1922419a8c60
Vijay Parmar <vijay.parmar@scalybee.com>
2023-06-30 16:53:52 +0530

    TICKET ES-1490

---
+++ app/Api/Repositories/WOTRepository.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Laraspace\Api\Repositories;
+
+use DB;
+use Laraspace\Models\Map;
+use Laraspace\Models\Location;
+use Laraspace\Models\Contact;
+use Laraspace\Models\Itinerary;
+use Laraspace\Models\Page;
+use Laraspace\Models\Website;
+
+class WOTRepository
+{
+    public function __construct()
+    { 
+        //
+    }
+
+    public function getWebsiteId($request)
+    {
+      $website = Website::where('linked_tournament', $request->tournamentId)->first();
+      return $website;
+    }
+
+    public function getAllLocations($request)
+    {
+      $locations = Location::where('website_id', $request->websiteId)->orderBy('order')->get();
+      return $locations;
+    }
+
+    public function getAllMarkers($request)
+    {
+        $markers = Map::where('website_id', $request->websiteId)->get();
+        return $markers;
+    }
+
+    public function getContact($request)
+    {
+        $contact = Contact::where('website_id', $request->websiteId)->first();
+        return $contact;
+    }
+
+    public function getAccommodation($request)
+    {
+        $pages = Page::select([
+            'id', 
+            'name', 
+            'title', 
+            'content', 
+            'is_enabled', 
+            'is_published'
+        ])->where('name', 'accommodation')->where('website_id', $request->websiteId)->first();
+
+        return $pages;
+    }
+
+    public function getVisitors($request)
+    {
+        $pages = Page::select([
+            'id', 
+            'name', 
+            'title', 
+            'content', 
+            'is_enabled', 
+            'is_published'
+        ])->where('name', 'visitors')->where('website_id', $request->websiteId)->first();
+
+        return $pages;
+    }
+
+    public function getTouristInformation($request)
+    {
+        $pages = Page::select([
+            'id', 
+            'name', 
+            'title', 
+            'content', 
+            'is_enabled', 
+            'is_published'
+        ])->where('name', 'tourist_information')->where('website_id', $request->websiteId)->first();
+
+        return $pages;
+    }
+
+    public function getTips($request)
+    {
+        $pages = Page::select([
+            'id', 
+            'name', 
+            'title', 
+            'content', 
+            'is_enabled', 
+            'is_published'
+        ])->where('name', 'tips')->where('website_id', $request->websiteId)->first();
+
+        return $pages;
+    }
+
+    public function getProgram($request)
+    {
+        $pages = Page::select([
+            'id', 
+            'name', 
+            'title', 
+            'content', 
+            'is_enabled', 
+            'is_published'
+        ])->where('name', 'program')->where('website_id', $request->websiteId)->first();
+
+        $itineraries = Itinerary::with(['items' => function($query){
+            $query->orderBy('order');
+          }])->where('website_id', $request->websiteId)->orderBy('order')->get();
+
+        return $itineraries;
+    }
+
+
+}
---
+++ app/Api/Services/WOTService.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Laraspace\Api\Services;
+
+use Laraspace\Api\Contracts\WOTContract;
+use Laraspace\Api\Repositories\WOTRepository;
+
+class WOTService implements WOTContract
+{
+    /**
+     * @var WOTRepository
+     */
+    protected $wotRepo;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param WOTRepository $wotRepo
+     */
+    public function __construct()
+    {
+        $this->wotRepo = new \Laraspace\Api\Repositories\WOTRepository();
+    }
+
+    /*
+    * Get locations
+    *
+    * @return response
+    */
+    public function getWebsiteId($request)
+    {
+        $data = $this->wotRepo->getWebsiteId($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+    
+    /*
+    * Get locations
+    *
+    * @return response
+    */
+    public function getLocations($request)
+    {
+        $data = $this->wotRepo->getAllLocations($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get markers
+    *
+    * @return response
+    */
+    public function getMarkers($request)
+    {
+        $data = $this->wotRepo->getAllMarkers($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get contact
+    *
+    * @return response
+    */
+    public function getContact($request)
+    {
+        $data = $this->wotRepo->getContact($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get Accommodation
+    *
+    * @return response
+    */
+    public function getAccommodation($request)
+    {
+        $data = $this->wotRepo->getAccommodation($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get getVisitors
+    *
+    * @return response
+    */
+    public function getVisitors($request)
+    {
+        $data = $this->wotRepo->getVisitors($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get getTouristInformation
+    *
+    * @return response
+    */
+    public function getTouristInformation($request)
+    {
+        $data = $this->wotRepo->getTouristInformation($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get getTips
+    *
+    * @return response
+    */
+    public function getTips($request)
+    {
+        $data = $this->wotRepo->getTips($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+
+    /*
+    * Get getProgram
+    *
+    * @return response
+    */
+    public function getProgram($request)
+    {
+        $data = $this->wotRepo->getProgram($request);
+
+        return ['data' => $data, 'status_code' => '200', 'message' => 'All data'];
+    }
+}
