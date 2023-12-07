1e73e05ece09a61709b2c4eff936a291d05e55c8
Shift <shift@laravelshift.com>
2023-12-07 08:38:30 +0000

    Inject new request parameter

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -81,10 +81,11 @@ class ResetPasswordController extends Controller
     /**
      * Get the response for a successful password reset.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @param  string  $response
      * @return \Illuminate\Http\RedirectResponse
      */
-    protected function sendResetResponse($response)
+    protected function sendResetResponse(Request $request, $response)
     {
         return redirect($this->redirectPath())
             ->with('status', trans($response));
@@ -94,6 +95,7 @@ class ResetPasswordController extends Controller
      * Get the response for a failed password reset.
      *
      * @param  \Illuminate\Http\Request
+     * @param  \Illuminate\Http\Request  $request
      * @param  string  $response
      * @return \Illuminate\Http\RedirectResponse
      */
